6940f7b322d0fe35d0d010cbae320e76198e3dc2
Savino Jossi <3669673+sjossi@users.noreply.github.com>
2022-09-26 16:15:42 -0400

    update UF2 unpacker test

--- ofrak_components/ofrak_components/uf2.py
+++ ofrak_components/ofrak_components/uf2.py
@@ -1,21 +1,83 @@
+import io
 import logging
-from uf2 import uf2conv
+import struct
 from dataclasses import dataclass
 
 from ofrak.resource import Resource
+from ofrak.resource_view import ResourceView
+from ofrak.model.resource_model import ResourceAttributes
 from ofrak.component.unpacker import Unpacker
 from ofrak.component.packer import Packer
 from ofrak.component.identifier import Identifier
+from ofrak.component.analyzer import Analyzer
 from ofrak.core.binary import GenericBinary
 from ofrak_type.range import Range
+from ofrak_type.endianness import Endianness
+from ofrak_io.deserializer import BinaryDeserializer
+from ofrak.model.resource_model import index
+from ofrak.service.resource_service_i import ResourceFilter
 
 LOGGER = logging.getLogger(__name__)
 
+UF2_MAGIC_START_ONE = 0x0A324655
+UF2_MAGIC_START_TWO = 0x9E5D5157
+UF2_MAGIC_END = 0x0AB16F30
+
+HEADER_LENGTH = 32
+DATA_LENGTH = 476
+
 
 @dataclass
-class Uf2Data(GenericBinary):
+class Uf2File(GenericBinary):
     """
-    A UF2 binary blob
+    A UF2 file
+    """
+
+
+@dataclass
+class Uf2Block(ResourceView):
+    """
+    A UF2 block in a UF2 file
+    """
+
+
+@dataclass
+class Uf2BlockHeader(ResourceAttributes):
+    """
+    Recreates the official spec
+
+    Offset  Size    Value
+    0       4       First magic number, 0x0A324655 ("UF2\n")
+    4       4       Second magic number, 0x9E5D5157
+    8       4       Flags
+    12      4       Address in flash where the data should be written
+    16      4       Number of bytes used in data (often 256)
+    20      4       Sequential block number; starts at 0
+    24      4       Total number of blocks in file
+    28      4       File size or board family ID or zero
+    32      476     Data, padded with zeros
+    508     4       Final magic number, 0x0AB16F30
+    """
+
+    flags: int
+    target_addr: int
+    payload_size: int
+    flags: int
+    target_addr: int
+    payload_size: int
+    block_no: int
+    num_blocks: int
+    filesize_family_id: int
+
+    @index
+    def BlockNo(self) -> int:
+        return self.block_no
+
+
+@dataclass
+class Uf2BlockData(GenericBinary):
+    """
+    Data in the payload section of a UF2 block
     """
 
 
@@ -26,8 +88,8 @@ class Uf2Unpacker(Unpacker[None]):
     Extracts the data from a UF2 packed file.
     """
 
-    targets = (Uf2Data,)
-    children = (GenericBinary,)
+    targets = (Uf2File,)
+    children = (Uf2Block,)
 
     async def unpack(self, resource: Resource, config=None):
         """
@@ -35,21 +97,37 @@ class Uf2Unpacker(Unpacker[None]):
 
         UF2 files contain blocks of binary data.
         """
+        data_length = await resource.get_data_length()
 
-        # TODO: technically there's multiple types of data. At least
-        # investigate what they mean - sj
-
-        resource_data = await resource.get_data()
-        uf2_data = uf2conv.convert_from_uf2(resource_data)
-        await resource.create_child(tags=(GenericBinary,), data=uf2_data)
+        for i in range(0, data_length, 512):
+            block_r = await resource.create_child(tags=(Uf2Block,), data_range=Range(i, i + 512))
+            block_attributes = await block_r.analyze(Uf2BlockHeader)
+            block_r.add_attributes(block_attributes)
 
 
-class Uf2Packer(Packer[None]):
+class Uf2BlockUnpacker(Unpacker[None]):
+    """
+    UF2 Block unpacker.
+
+    Extracts the data from a UF2 Block.
+    """
+
+    targets = (Uf2Block,)
+    children = (Uf2BlockData,)
+
+    async def unpack(self, resource: Resource, config=None):
+        await resource.create_child(
+            tags=(Uf2BlockData,), data_range=Range(HEADER_LENGTH, HEADER_LENGTH + DATA_LENGTH)
+        )
+
+
+class Uf2FilePacker(Packer[None]):
     """
     Pack a resource into the UF2 file format
     """
 
-    targets = (Uf2Data,)
+    id = b"Uf2FilePacker"
+    targets = (Uf2File,)
 
     async def pack(self, resource: Resource, config=None):
         """
@@ -58,17 +136,102 @@ class Uf2Packer(Packer[None]):
         :param resource:
         :param config:
         """
-        uf2_child = await resource.get_only_child()
-        uf2_packed = uf2conv.convert_to_uf2(await uf2_child.get_data())
-        original_size = await resource.get_data_length()
-        resource.queue_patch(Range(0, original_size), uf2_packed)
+        repacked_data = b""
+
+        for uf2_block in await resource.get_children(
+            r_filter=ResourceFilter(
+                Uf2Block,
+            )
+        ):
+            uf2_data = await uf2_block.get_data()
+            repacked_data += uf2_data
 
 
-class Uf2Identifier(Identifier):
+class Uf2BlockPacker(Packer[None]):
+    """
+    Pack a resource into the UF2 file format
+    """
+
+    id = b"Uf2BlockPacker"
+    targets = (Uf2Block,)
+
+    async def pack(self, resource: Resource, config=None):
+        repacked_data = b""
+
+        attributes = resource.get_attributes(Uf2BlockHeader)
+        header_data = struct.pack(
+            "8I",
+            UF2_MAGIC_START_ONE,
+            UF2_MAGIC_START_TWO,
+            attributes.flags,
+            attributes.target_addr,
+            attributes.payload_size,
+            attributes.block_no,
+            attributes.num_blocks,
+            attributes.filesize_family_id,
+        )
+
+        payload = await resource.get_only_child()
+        payload_data = await payload.get_data()
+
+        repacked_data += header_data
+        repacked_data += payload_data
+        repacked_data += bytes(UF2_MAGIC_END)
+
+        LOGGER.info(repacked_data)
+
+
+class Uf2BlockAnalyzer(Analyzer[None, Uf2Block]):
+    """
+    Analyze the Uf2Blocks of a Uf2File
+    """
+
+    targets = (Uf2Block,)
+    outputs = (Uf2BlockHeader,)
+
+    async def analyze(self, resource: Resource, config=None) -> Uf2BlockHeader:
+        data = await resource.get_data()
+        deserializer = BinaryDeserializer(
+            io.BytesIO(data),
+            endianness=Endianness.LITTLE_ENDIAN,
+            word_size=4,
+        )
+
+        # TODO: add friendly name for family_id
+
+        deserialized = deserializer.unpack_multiple("8I476sI")
+        (
+            magic_start_one,
+            magic_start_two,
+            flags,
+            target_addr,
+            payload_size,
+            block_no,
+            num_blocks,
+            filesize_familyID,
+            data,
+            magic_end,
+        ) = deserialized
+
+        assert magic_start_one == UF2_MAGIC_START_ONE
+        assert magic_start_two == UF2_MAGIC_START_TWO
+        assert magic_end == UF2_MAGIC_END
+
+        return Uf2BlockHeader(
+            flags,
+            target_addr,
+            payload_size,
+            block_no,
+            num_blocks,
+            filesize_familyID,
+        )
+
+
+class Uf2FileIdentifier(Identifier):
     id = b"Uf2FileIdentifier"
     targets = (GenericBinary,)
 
     async def identify(self, resource: Resource, config=None):
-        resource_data = await resource.get_data()
-        if uf2conv.is_uf2(resource_data):
-            resource.add_tag(Uf2Data)
+        resource_data = await resource.get_data(Range(0, 8))
+        if resource_data[:4] == UF2_MAGIC_START_ONE and resource_data[4:8] == UF2_MAGIC_START_TWO:
+            resource.add_tag(Uf2File)
--- ofrak_components/ofrak_components_test/test_uf2_component.py
+++ ofrak_components/ofrak_components_test/test_uf2_component.py
@@ -1,48 +1,53 @@
-from uf2 import uf2conv
 import logging
+from pathlib import Path
 
-from ofrak import OFRAKContext
-from ofrak.core.binary import GenericBinary
+from ofrak.ofrak_context import OFRAKContext
 from ofrak.resource import Resource
-from ofrak_components.uf2 import Uf2Data
+from ofrak.service.resource_service_i import ResourceFilter, ResourceAttributeValueFilter
+from ofrak_components.uf2 import Uf2File, Uf2BlockData, Uf2BlockHeader
 from ofrak.core.strings import StringPatchingModifier, StringPatchingConfig
+import ofrak_components_test
 
 from pytest_ofrak.patterns.unpack_modify_pack import UnpackModifyPackPattern
 
 LOGGER = logging.getLogger(__name__)
 
-INITIAL_DATA = b"Hello World"
-EXPECTED_DATA = b"Hello OFRAK" + b"\x00" * 245
-TARGET_UF2_FILE = "hello_uf2.uf2"
-UF2_ENTRY_NAME = "hello_uf2_file"
+FILENAME = "rp2-pico-20220618-v1.19.1.uf2"
+EXPECTED_DATA = b"Raspberry Pi Pico with RP1337"
+# TARGET_UF2_FILE = "hello_uf2.uf2"
+# UF2_ENTRY_NAME = "hello_uf2_file"
 
 
 class TestUf2UnpackModifyPack(UnpackModifyPackPattern):
     async def create_root_resource(self, ofrak_context: OFRAKContext) -> Resource:
-        packed_data = uf2conv.convert_to_uf2(INITIAL_DATA)
-        root_resource = await ofrak_context.create_root_resource("uf2", packed_data)
-        root_resource.add_tag(GenericBinary)
+        asset_path = Path(ofrak_components_test.ASSETS_DIR, FILENAME)
+        root_resource = await ofrak_context.create_root_resource_from_file(asset_path)
+        root_resource.add_tag(Uf2File)
         await root_resource.save()
         return root_resource
 
     async def unpack(self, uf2_resource: Resource) -> None:
         await uf2_resource.unpack()
-        assert uf2_resource.has_tag(Uf2Data), "Expected resource to have tag Uf2Data"
+        assert uf2_resource.has_tag(Uf2File), "Expected resource to have tag Uf2File"
 
     async def modify(self, unpacked_uf2_resource: Resource) -> None:
-        uf2_r = await unpacked_uf2_resource.get_only_child()
-        string_patch_config = StringPatchingConfig(6, "OFRAK")
-        await uf2_r.run(StringPatchingModifier, string_patch_config)
-        LOGGER.info(await uf2_r.get_data())
+        block = await unpacked_uf2_resource.get_only_child(
+            r_filter=ResourceFilter(
+                attribute_filters=(ResourceAttributeValueFilter(Uf2BlockHeader.BlockNo, 0x3C7),)
+            )
+        )
+        await block.unpack()
+        block_data = await block.get_only_child(r_filter=ResourceFilter.with_tags(Uf2BlockData))
+        string_patch_config = StringPatchingConfig(
+            offset=0xAC, string="RP1337", null_terminate=False
+        )
+        await block_data.run(StringPatchingModifier, string_patch_config)
 
     async def repack(self, uf2_resource: Resource) -> None:
-        LOGGER.info(await uf2_resource.get_data())
         await uf2_resource.pack()
 
     async def verify(self, repacked_uf2_resource: Resource) -> None:
         resource_data = await repacked_uf2_resource.get_data()
-        unpacked_data = uf2conv.convert_from_uf2(resource_data)
-        LOGGER.info(len(unpacked_data))
-        LOGGER.info(len(EXPECTED_DATA))
+        unpacked_data = resource_data[0x78EB5:0x78ED2]
         assert unpacked_data == EXPECTED_DATA
-        assert repacked_uf2_resource.has_tag(Uf2Data)
+        assert repacked_uf2_resource.has_tag(Uf2File)
