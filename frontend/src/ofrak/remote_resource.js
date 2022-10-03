bd8d1506a8e04517e8cc5bd3bdcb099887495565
Wyatt <whyitfor@noreply.users.github.com>
2022-10-03 13:29:26 -0400

    Fix path of StringFindReplaceConfig in remote_resource.js

--- frontend/src/ofrak/remote_resource.js
+++ frontend/src/ofrak/remote_resource.js
@@ -141,7 +141,7 @@ export class RemoteResource extends Resource {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(
-        ["ofrak_components.string.StringFindReplaceConfig", {
+        ["ofrak.core.strings.StringFindReplaceConfig", {
           to_find: to_find,
           replace_with: replace_with,
           null_terminate: null_terminate,
