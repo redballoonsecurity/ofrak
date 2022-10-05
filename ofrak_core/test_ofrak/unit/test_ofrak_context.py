571075e08e0d9a6ea4ed3d662cb64528eb935e48
Wyatt <53830972+whyitfor@users.noreply.github.com>
2022-10-05 00:03:45 -0400

    Add a test for OFRAK.run.

---
+++ ofrak_core/test_ofrak/unit/test_ofrak_context.py
@@ -0,0 +1,15 @@
+from ofrak import OFRAK, OFRAKContext
+
+
+def test_ofrak_context():
+    """
+    Test that OFRAK.run successfully creates an event loop and runs the target async function.
+    """
+
+    async def main(ofrak_context: OFRAKContext, binary: bytes):
+        resource = await ofrak_context.create_root_resource("test_binary", binary)
+        data = await resource.get_data()
+        assert data == binary
+
+    ofrak = OFRAK()
+    ofrak.run(main, b"Hello world\n")
