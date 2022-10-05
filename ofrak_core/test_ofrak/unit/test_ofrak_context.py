277731db59d801bf2ee75b246c8b7960b22981d7
Wyatt <53830972+whyitfor@users.noreply.github.com>
2022-10-05 01:10:57 -0400

    Try resetting event loop

--- ofrak_core/test_ofrak/unit/test_ofrak_context.py
+++ ofrak_core/test_ofrak/unit/test_ofrak_context.py
@@ -1,21 +1,14 @@
 import asyncio
 
-import pytest
-
 from ofrak import OFRAK, OFRAKContext
-
-
-@pytest.fixture
-def event_loop():
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
 
 
 def test_ofrak_context():
     """
     Test that OFRAK.run successfully creates an event loop and runs the target async function.
     """
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
     async def main(ofrak_context: OFRAKContext, binary: bytes):
         resource = await ofrak_context.create_root_resource("test_binary", binary)
