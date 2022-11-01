2e334e5fcb030c461332e9b6f6b39f537b0bd19f
Wyatt <53830972+whyitfor@users.noreply.github.com>
2022-10-31 20:19:00 -0400

    Update ofrak_core/ofrak/service/serialization/serializers/__init__.py
    Update comment

--- ofrak_core/ofrak/service/serialization/serializers/__init__.py
+++ ofrak_core/ofrak/service/serialization/serializers/__init__.py
@@ -1,6 +1,6 @@
-# Choosing the "Make Users Suffer" option pending resolution of
+# Suppress all BeartypeDecorHintPep585DeprecationWarning warnings pending resolution of
 # https://github.com/redballoonsecurity/ofrak/issues/92.
-# This is actually nicer for OFRAK users, who don't want to see these warnings.
+# This is nice for OFRAK users, who don't want to see these warnings.
 from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
 from warnings import filterwarnings
 
