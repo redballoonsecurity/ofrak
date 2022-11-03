cc94d66f98284d1e0e7a95fc7ef2f94b00e1d7b7
Wyatt <53830972+whyitfor@users.noreply.github.com>
2022-11-03 17:06:26 -0400

    Add tests for UImageHeader, UImageMultiHeader (#97)
    * Add tests for UImageHeader, UImageMultiHeader
    
    - Add tests for UImageHeader, UImageMultiHeader, UImageProgramAttributesAnalyzer
    - Add UImage.get_multi_header method
    
    Bump ofrak_components test coverage from 89 to 96
    
    * Bump ofrak_components expected function coverage to 99%

--- ofrak_components/ofrak_components_test/test_uimage_component.py
+++ ofrak_components/ofrak_components_test/test_uimage_component.py
@@ -4,13 +4,20 @@ import subprocess
 import pytest
 
 from ofrak import OFRAKContext
+from ofrak.core import ProgramAttributes
 from ofrak.resource import Resource
 from ofrak_components.uimage import (
     UImage,
     UImageHeaderModifierConfig,
     UImageHeaderModifier,
+    UImageOperatingSystem,
+    UImageArch,
+    UImageCompressionType,
+    UImageType,
+    UImageMultiHeader,
 )
 import test_ofrak.components
+from ofrak_type.error import NotFoundError
 
 NEW_UIMAGE_NAME = "new image name"
 UIMAGE_TESTFILE_PATHS = [
@@ -24,14 +31,18 @@ UIMAGE_TESTFILE_PATHS = [
 ]
 
 
-@pytest.mark.parametrize("test_case", UIMAGE_TESTFILE_PATHS)
-async def test_uimage_unpack_modify_pack(test_case, ofrak_context, tmpdir):
+@pytest.fixture(params=UIMAGE_TESTFILE_PATHS)
+async def uimage_resource(ofrak_context, request) -> Resource:
+    uimage_path = os.path.join(test_ofrak.components.ASSETS_DIR, request.param)
+    return await ofrak_context.create_root_resource_from_file(uimage_path)
+
+
+async def test_uimage_unpack_modify_pack(uimage_resource: Resource, tmpdir):
     """Test unpacking, modifying and then repacking a UImage file."""
-    root_resource = await create_root_resource(ofrak_context, test_case)
-    await root_resource.unpack_recursively()
-    await modify(root_resource)
-    await root_resource.pack_recursively()
-    await verify(root_resource, tmpdir)
+    await uimage_resource.unpack_recursively()
+    await modify(uimage_resource)
+    await uimage_resource.pack_recursively()
+    await verify(uimage_resource, tmpdir)
 
 
 async def create_root_resource(ofrak_context: OFRAKContext, path) -> Resource:
@@ -73,3 +84,39 @@ async def verify(repacked_root_resource: Resource, tmpdir) -> None:
         "utf-8"
     )
     assert f"Image Name:   {NEW_UIMAGE_NAME}" in stdout, stdout
+
+
+async def test_uimage_header(uimage_resource: Resource) -> None:
+    """
+    Test that UImageHeader and UImageMultiHeader methods return expected types.
+    """
+    await uimage_resource.unpack()
+    uimage = await uimage_resource.view_as(UImage)
+    header = await uimage.get_header()
+    assert isinstance(header.get_os(), UImageOperatingSystem)
+    assert isinstance(header.get_arch(), UImageArch)
+    assert isinstance(header.get_compression_type(), UImageCompressionType)
+    assert isinstance(header.get_type(), UImageType)
+    assert isinstance(header.get_name(), str)
+    assert isinstance(header.get_data_size(), int)
+    assert isinstance(header.get_load_vaddr(), int)
+    assert isinstance(header.get_entry_point_vaddr(), int)
+
+    header_type = header.get_type()
+    if header_type is UImageType.MULTI:
+        multi_header = await uimage.get_multi_header()
+        assert isinstance(multi_header, UImageMultiHeader)
+        assert isinstance(multi_header.get_number_of_bodies(), int)
+        assert all([isinstance(size, int) for size in multi_header.get_image_sizes()])
+    else:
+        with pytest.raises(NotFoundError):
+            _ = await uimage.get_multi_header()
+
+
+async def test_uimage_program_attributes_analzyer(uimage_resource: Resource) -> None:
+    """
+    Test that UImageProgramAttributesAnalyzer returns ProgramAttributes.
+    """
+    await uimage_resource.unpack()
+    program_attributes = await uimage_resource.analyze(ProgramAttributes)
+    assert isinstance(program_attributes, ProgramAttributes)
