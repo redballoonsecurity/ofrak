f9e8c64b51f769e782a1750128403bbc0d325efd
Wyatt <53830972+whyitfor@users.noreply.github.com>
2022-12-08 00:25:15 -0500

    Update test_ghidra_config.py

--- disassemblers/ofrak_ghidra/ofrak_ghidra_test/test_ghidra_config.py
+++ disassemblers/ofrak_ghidra/ofrak_ghidra_test/test_ghidra_config.py
@@ -1,22 +1,11 @@
-from ofrak_ghidra.config import ofrak_ghidra_config
+from dataclasses import dataclass
 
+import pytest
 
-def test_ofrak_ghidra_config():
-    EXPECTED_YAML = """ghidra_install:
-  log_file: /tmp/test_ghidra.log
-  path: /tmp/test_ghidra
-server:
-  analysis:
-    host: TEST_ANALYSIS_HOST
-    port: 1337
-  pass: hunter2
-  repository:
-    host: TEST_REPO_HOST
-    port: 666
-  user: testuser
-"""
+from ofrak_ghidra.config.__main__ import _dump_config, _import_config, _restore_config
+from ofrak_ghidra.config.ofrak_ghidra_config import load_ghidra_config
 
-    DEFAULT_YAML = """ghidra_install:
+DEFAULT_CONFIG = """ghidra_install:
   log_file: ~/.ghidra/.ghidra_10.1.2_PUBLIC/application.log
   path: /opt/rbs/ghidra_10.1.2_PUBLIC
 server:
@@ -30,62 +19,86 @@ server:
   user: root
 """
 
-    DEFAULT_HELP = """
-python -m ofrak_ghidra.config dump
-  Dumps the current OFRAK Ghidra config as yaml to stdout.
-
-python -m ofrak_ghidra.config import <config-path>
-  Loads a complete OFRAK Ghidra config from a path to a yaml file and saves it as the current
-  Ghidra config.
-
-python -m ofrak_ghidra.config restore
-  Restore the default OFRAK Ghidra settings.
-
-To change one or more of the options, the recommended process is:
-1. Use `dump` to save the current settings to a temporary yaml file
-2. Edit the temporary yaml file, changing settings as desired
-3. Use `import` to then load that temporary yaml file
-
-The options are:
-
-ghidra_install:
-  path: # Path to the root directory of the Ghidra install, e.g. /opt/rbs/ghidra_10.1.2_PUBLIC
-  log_file: # Path to the file that Ghidra will use for its logs
-
+MODIFIED_CONFIG = """ghidra_install:
+  log_file: /tmp/test_ghidra.log
+  path: /tmp/test_ghidra
 server:
-  user: # User for the local Ghidra repository and server OFRAK will create
-  pass: # Password for the local Ghidra repository and server OFRAK will create
-  repository:
-    host: # Host for the Ghidra repository OFRAK will create, e.g. ghidra://localhost
-    port: # Port for the Ghidra repository OFRAK will create
   analysis:
-    host: # Host for the server OFRAK will create in a headless Ghidra instance, e.g. http://localhost
-    port: # Host for the port OFRAK will create in a headless Ghidra instance
+    host: TEST_ANALYSIS_HOST
+    port: 1337
+  pass: hunter2
+  repository:
+    host: TEST_REPO_HOST
+    port: 666
+  user: testuser
 """
 
-    new_ghidra_config = ofrak_ghidra_config.load_ghidra_config()
 
-    new_ghidra_config.ghidra_path = "/tmp/test_ghidra"
-    new_ghidra_config.ghidra_log_file = "/tmp/test_ghidra.log"
-    new_ghidra_config.ghidra_server_user = "testuser"
-    new_ghidra_config.ghidra_server_pass = "hunter2"
-    new_ghidra_config.ghidra_analysis_host = "TEST_ANALYSIS_HOST"
-    new_ghidra_config.ghidra_analysis_port = 1337
-    new_ghidra_config.ghidra_repository_host = "TEST_REPO_HOST"
-    new_ghidra_config.ghidra_repository_port = 666
+@dataclass
+class Args:
+    config_path: str
 
-    ofrak_ghidra_config.save_ghidra_config(new_ghidra_config)
-    test_ghidra_config = ofrak_ghidra_config.load_ghidra_config()
-    test_ghidra_config_yaml = test_ghidra_config.to_yaml()
 
-    assert test_ghidra_config_yaml == EXPECTED_YAML
+@pytest.fixture
+def restore_config_after_test():
+    """
+    Make sure default config is restored at end of test.
+    """
+    yield
+    _restore_config(None)
 
-    ofrak_ghidra_config.restore_default_ghidra_config()
-    default_ghidra_config = ofrak_ghidra_config.load_ghidra_config()
-    default_ghidra_config_yaml = default_ghidra_config.to_yaml()
 
-    assert default_ghidra_config_yaml == DEFAULT_YAML
+def test_dump_config(capsys):
+    """
+    Test that ofrak_ghira.config dump returns expected output.
+    """
+    _validate_config(capsys, DEFAULT_CONFIG)
 
-    default_config_help = default_ghidra_config.config_help()
 
-    assert default_config_help == DEFAULT_HELP
+def test_import_config(capsys, tmp_path, restore_config_after_test):
+    """
+    Test that importing a config changes the default config.
+    """
+    # Validate default config is set
+    _validate_config(capsys, DEFAULT_CONFIG)
+
+    # Change the default config
+    path = tmp_path / "config.yml"
+    with open(path, "w") as f:
+        f.write(MODIFIED_CONFIG)
+    _import_config(Args(path))
+
+    # Assert config is changed
+    _validate_config(capsys, MODIFIED_CONFIG)
+
+
+def test_restore_config(capsys, tmp_path, restore_config_after_test):
+    """
+    Test that "ofrak_ghidra.config restore" restores the default config file.
+    """
+    # Change the default config
+    path = tmp_path / "config.yml"
+    with open(path, "w") as f:
+        f.write(MODIFIED_CONFIG)
+    _import_config(Args(path))
+
+    # Assert config is modified config
+    _validate_config(capsys, MODIFIED_CONFIG)
+
+    # Restore config and validate that it is restored
+    _restore_config(None)
+    _validate_config(capsys, DEFAULT_CONFIG)
+
+
+def _validate_config(capsys, expected_config: str):
+    _dump_config(None)
+    captured = capsys.readouterr()
+    assert captured.out == expected_config + "\n"
+
+
+def test_ofrak_ghidra_config_help():
+    """
+    Assert that OFRAKGhidraConfig.config_help returns a help string.
+    """
+    config = load_ghidra_config()
+    assert isinstance(config.config_help(), str)
