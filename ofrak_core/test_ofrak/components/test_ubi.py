72ac0c28c3a3fe09097f12b2169f859368cf8673
Kibo <kibo@redballoonsecurity.com>
2023-01-18 12:10:05 -0500

    Tighten ubi volume test (volume hashes are expected and are explicitly sorted by volume id)

--- ofrak_core/test_ofrak/components/test_ubi.py
+++ ofrak_core/test_ofrak/components/test_ubi.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 
 import pytest
@@ -9,17 +10,17 @@ from pytest_ofrak.patterns.compressed_filesystem_unpack_modify_pack import Compr
 
 from test_ofrak.components import ASSETS_DIR
 
-from ofrak import OFRAKContext
+from ofrak import OFRAKContext, ResourceSort
 from ofrak.resource import Resource
+from ofrak.core.checksum import Md5Attributes, Sha256Attributes
 
-TEST_DATA = b"$ base64 -d <<< f0VMRuH//xAICIDSEAAAFAIAtwABAAAABAAAAAEAAAAcAAAAAAAAAAAA\
+TEST_PAYLOAD = b"$ base64 -d <<< f0VMRuH//xAICIDSEAAAFAIAtwABAAAABAAAAAEAAAAcAAAAAAAAAAAA\
 AAAAAAAAAQAAAEAAOAABAADU8v//FwAAAADy//8XAAAAAIIAgNL6//8X > aarch64.elf;EOF"
 
-EXPECTED_OUTPUTS = [
-    b'hsqs\x94\x04\x00\x00\x8dd\x13^\x00\x00\x04\x00\x15\x00\x00\x00\x04\x00\x12\x00\xc0\x06\x01\x00\x04\x00\x00\x00a\x17\xc4\x18\x00\x00\x00\x00\x020\x1c\x00\x00\x00\x00\x00\xfa/\x1c\x00\x00\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xffv\xdf\x1b\x00\x00\x00\x00\x00\xa8\xfd\x1b\x00\x00\x00\x00\x00\xf0)\x1c\x00\x00\x00\x00\x00\xe4/\x1c\x00\x00\x00\x00\x00\x0c\x80\x00\x00\x04\x00\x1c\x00\t\x00\x90\x00@\x00\xfd7zXZ\x00\x00\x01i"\xde6\x04\xc1\x9f\xe3\x06\x80\x80\x10\x07\x00!\x01\x0c\x00\x00\x00\x8e1\x91\xc6\xe2As\xef\xfel\x00?\x91E\x84`\x0e\xfcJV\xfc\xf5\xd4\x8a\x1d\xf8\xbb\xeb\xf6#\xbco\x0e\xf0\x136\xef7\xee\xbf\xc9\xbb\xe9\xfa\x9cP\x92\x1c\xf2N\xac\xf3\x03*\xbd\x95%cu|\x82|\x96\xb2\x1e\xb3t\x98.\xe9\x13?\xec:\xcfr(\xe09Q;<\xd9\xda\x8a\x1b\xfe\xc2d\nKn\xce\xa6&\x11C\x86\x8e\x1c\xa8U\x7f\x8d\xed\xabx\xd6j\xbb\xa0\xc0\x91\x9a\xcb\'B\x0b',
-    b'',
-    b'$ base64 -d <<< f0VMRuH//xAICIDSEAAAFAIAtwABAAAABAAAAAEAAAAcAAAAAAAAAAAAAAAAAAAAAQAAAEAAOAABAADU8v//FwAAAADy//8XAAAAAIIAgNL6//8X > aarch64.elf;EOF\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-]
+EXPECTED_HASHES = ('65917699e92f9ae514f2aea0606a462263f5595360e12c41f8014d84238519b5',
+                   'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+                   '340d8293708dc682906f52087d92728fda28f73ddfcf0fa1c17121a823df0c73')
+
 class TestUbiUnpackModifyPack(CompressedFileUnpackModifyPackPattern):
     async def create_root_resource(self, ofrak_context: OFRAKContext) -> Resource:
         """
@@ -47,7 +48,7 @@ class TestUbiUnpackModifyPack(CompressedFileUnpackModifyPackPattern):
         )
         await root_resource.create_child_from_view(
             ubi_vol_resource,
-            data=TEST_DATA
+            data=TEST_PAYLOAD
         )
         await root_resource.save()
 
@@ -59,9 +60,11 @@ class TestUbiUnpackModifyPack(CompressedFileUnpackModifyPackPattern):
         await root_resource.unpack()
         tree = await root_resource.summarize_tree()
 
-        for child in await root_resource.get_children():
-            if not (await child.get_data()) in EXPECTED_OUTPUTS:
-                print("Not in EXPECTED OUTPUTS")
+        for ubi_vol in await root_resource.get_children(r_sort=ResourceSort(UbiVolume.UbiVolumeId)):
+            ubi_vol_view = await  ubi_vol.view_as(UbiVolume)
+            ubi_vol_data = await ubi_vol.get_data()
+            ubi_vol_id = ubi_vol_view.UbiVolumeId
 
-
-        print()
+            ubi_vol_hash = hashlib.sha256()
+            ubi_vol_hash.update(ubi_vol_data)
+            assert ubi_vol_hash.hexdigest() == EXPECTED_HASHES[ubi_vol_id]
