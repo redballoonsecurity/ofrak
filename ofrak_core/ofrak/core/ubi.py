c2a69d99164a5313929ef8b7844635e3bdf1b78e
Kibo <kibo@redballoonsecurity.com>
2023-01-18 10:26:56 -0500

    Packer

--- ofrak_core/ofrak/core/ubi.py
+++ ofrak_core/ofrak/core/ubi.py
@@ -8,6 +8,7 @@ import os
 from ofrak import Identifier, Analyzer
 from ofrak.component.packer import Packer
 from ofrak.component.unpacker import Unpacker
+from ofrak.model.component_model import ComponentExternalTool
 from ofrak.resource import Resource
 from ofrak.core.filesystem import File, Folder, FilesystemRoot, SpecialFileType
 from ofrak.core.binary import GenericBinary
@@ -17,10 +18,18 @@ from ofrak_type.range import Range
 
 from ubireader import ubi_io
 from ubireader.ubi import ubi as ubireader_ubi
-from ubireader.ubi.defines import UBI_EC_HDR_MAGIC, UBI_VID_HDR_MAGIC
+from ubireader.ubi.defines import UBI_EC_HDR_MAGIC, UBI_VID_HDR_MAGIC, PRINT_VOL_TYPE_LIST
 from ubireader.utils import guess_leb_size, guess_peb_size
 
 LOGGER = logging.getLogger(__name__)
+
+UBINIZE_TOOL = ComponentExternalTool(
+    "ubinize",
+    "http://www.linux-mtd.infradead.org/faq/ubi.html#L_ubi_mkimg",
+    install_check_arg="--help",
+    apt_package="mtd-utils",
+    brew_package="",  # This isn't compatible with macos, but there may be an alternative tool to do the bidding.
+)
 
 
 @dataclass
@@ -31,7 +40,7 @@ class UbiVolume(ResourceView):
     peb_count: int  # size = UbiVolume.peb_count * Ubi.peb_size
     type: str
     name: str
-    flags: str
+    flag_autoresize: bool # `vol_flags` only specifies an auto-resize flag
     alignment: int
 
 
@@ -87,9 +96,9 @@ class UbiAnalyzer(Analyzer[None, Ubi]):
                 UbiVolume(
                     volume.vol_rec.rec_index,
                     volume.vol_rec.reserved_pebs,
-                    volume.vol_rec.vol_type,
-                    volume.vol_rec.name,
-                    volume.vol_rec.flags,
+                    PRINT_VOL_TYPE_LIST[volume.vol_rec.vol_type],
+                    volume.vol_rec.name.decode('utf-8'),  # TODO support unicode / make tunable?
+                    volume.vol_rec.flags,  # Autoresize flag for standard UBI
                     volume.vol_rec.alignment
                 )
             )
@@ -120,7 +129,7 @@ class UbiUnpacker(Unpacker[None]):
             temp_file.write(resource_data)
             temp_file.flush()
 
-            # extract temp_file to temp dir
+            # extract temp_file to temp_flush_dir
             with tempfile.TemporaryDirectory() as temp_flush_dir:
                 command = [
                     "ubireader_extract_images",
@@ -130,20 +139,19 @@ class UbiUnpacker(Unpacker[None]):
                 ]
                 subprocess.run(command, check=True, capture_output=True)
 
-                # read extracted files
                 ubi_view = await resource.view_as(Ubi)
 
-                ###
-
+                # Each file extracted by `ubireader_extract_images` is populated as an UbiVolume
+                # `ubireader_extract_images` incorrectly appends a `ubifs` suffix despite unpacking ubi images / volumes
                 for vol in ubi_view.volumes:
-                    f_path = f"{temp_flush_dir}/{os.path.basename(temp_file.name)}/img-{ubi_view.image_seq}_vol-{vol.name.decode('utf-8')}.ubifs"
+                    f_path = f"{temp_flush_dir}/{os.path.basename(temp_file.name)}" \
+                             f"/img-{ubi_view.image_seq}_vol-{vol.name}.ubifs"
                     print(f"path is: {f_path}")
                     with open(f_path, "rb") as f:
-                        await resource.create_child(
-                            tags=(UbiVolume,File),
+                        await resource.create_child_from_view(
+                            vol,
                             data=f.read()
                         )
-
 
 
 class UbiPacker(Packer[None]):
@@ -152,11 +160,57 @@ class UbiPacker(Packer[None]):
     """
 
     targets = (Ubi,)
-    external_dependencies = ()
+    external_dependencies = (UBINIZE_TOOL,)
 
     async def pack(self, resource: Resource, config=None) -> None:
-        print("Pretending to pack...")
-        pass
+        ubi_view = await resource.view_as(Ubi)
+
+        # with tempfile.NamedTemporaryFile(mode="rb") as temp:
+        with tempfile.TemporaryDirectory() as temp_flush_dir:
+            ubi_volumes = await resource.get_children()
+            command = [
+                "ubinize",
+                "-p",
+                str(ubi_view.peb_size),
+                "-m",
+                str(ubi_view.min_io_size),
+                "-o",
+                f"{temp_flush_dir}/output.ubi",
+                f"{temp_flush_dir}/config.ini"
+            ]
+            ubinize_ini_entries = []
+
+            for volume in ubi_volumes:
+                volume_view = await volume.view_as(UbiVolume)
+                volume_size = await volume.get_data_length()
+                if volume_size != 0:
+                    volume_path = f"{temp_flush_dir}/input-{ubi_view.image_seq}_vol-{volume_view.name}.ubivol"
+                    await volume.flush_to_disk(volume_path)
+                else:
+                    volume_path = None
+                    volume_size = (volume_view.peb_count - 1) * ubi_view.peb_size
+
+                ubinize_ini_entry = f"""\
+[{volume_view.name}-volume]
+mode=ubi
+{(f"image={volume_path}" if volume_path else "")}
+vol_id={volume_view.id}
+vol_size={volume_size}
+vol_type={volume_view.type}
+vol_name={volume_view.name}
+{(f"vol_flags=autoresize" if volume_view.flag_autoresize else "" )}
+"""
+                ubinize_ini_entries.append(ubinize_ini_entry)
+
+            with open(f"{temp_flush_dir}/config.ini", "w") as config_ini_file:
+                config_ini_file.write('\n'.join(ubinize_ini_entries))
+
+            subprocess.run(command, check=True, capture_output=True)
+
+            with open(f"{temp_flush_dir}/output.ubi", "rb") as output_f:
+                packed_blob_data = output_f.read()
+
+            resource.queue_patch(Range(0, await resource.get_data_length()), packed_blob_data)
 
 
 class UbiIdentifier(Identifier):
