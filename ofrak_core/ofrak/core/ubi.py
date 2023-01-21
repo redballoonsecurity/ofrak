5017681faaf8335add6747f3bf3eb3b3f19dda8a
edward <edward@redballoonsecurity.com>
2023-01-20 19:01:08 -0500

    fix linter

--- ofrak_core/ofrak/core/ubi.py
+++ ofrak_core/ofrak/core/ubi.py
@@ -50,6 +50,7 @@ class _PyLzoTool(ComponentExternalTool):
     def is_tool_installed(self) -> bool:
         try:
             import lzo  # type: ignore
+
             return True
         except ModuleNotFoundError:
             return False
