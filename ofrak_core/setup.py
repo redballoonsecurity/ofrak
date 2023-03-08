f82755a9ec529d554bb9745987662dbe29d693d9
Edward Larson <wasabiofip@gmail.com>
2023-03-08 15:23:05 -0500

    Release/ofrak 2.2.1 (#246)
    * remove pyyaml 6.0 pin
    
    * handle missing uber-apk-signer properly and os-agnostic
    
    * dockerstub goes to tmp dir with absolute path
    
    * bump changelog and version
    
    * bump version num in the GUI
    
    * update reedsolo pin
    
    * Bump ofrak_core requirements to latest versions
    
    * reword changelog
    
    ---------
    
    Co-authored-by: edward <edward@redballoonsecurity.com>
    Co-authored-by: Wyatt <53830972+whyitfor@users.noreply.github.com>

--- ofrak_core/setup.py
+++ ofrak_core/setup.py
@@ -40,7 +40,7 @@ def read_requirements(requirements_path):
 
 setuptools.setup(
     name="ofrak",
-    version="2.2.0",
+    version="2.2.1",
     description="A binary analysis and modification platform",
     packages=setuptools.find_packages(exclude=["test_ofrak", "test_ofrak.*"]),
     package_data={
