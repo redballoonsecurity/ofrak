68079bb8c29cf83cbc7478717faf1601ef6c577c
dannyp303 <danpesce94@gmail.com>
2023-03-16 11:14:50 -0400

    Add option to change backend URL in stores.js (#255)
    Co-authored-by: Dan Pesce <dan@redballoonsecurity.com>
    Co-authored-by: Jacob Strieb <99368685+rbs-jacob@users.noreply.github.com>

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -5,3 +5,9 @@ import { writable } from "svelte/store";
  */
 export const selected = writable(undefined);
 export const selectedResource = writable(undefined);
+
+/***
+ * Points the OFRAK frontend to a seperate backend server
+ * When empty, uses the same host and port as the frontend
+ */
+export const backendUrl = "";
