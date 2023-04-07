899d89b3380f1eda4ce0f6030a9c775234bbf25f
Marc Zalik <marc.zalik@gmail.com>
2023-04-07 15:46:57 -0400

    Add styling to Add Element button

--- frontend/src/ComponentConfigNode.svelte
+++ frontend/src/ComponentConfigNode.svelte
@@ -1,4 +1,23 @@
 <style>
+  button {
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+
+  button:hover,
+  button:focus {
+    outline: none;
+    box-shadow: inset 1px 1px 0 var(--main-fg-color),
+      inset -1px -1px 0 var(--main-fg-color);
+  }
+
+  button:active {
+    box-shadow: inset 2px 2px 0 var(--main-fg-color),
+      inset -2px -2px 0 var(--main-fg-color);
+  }
+
   .container {
     min-height: 100%;
     display: flex;
