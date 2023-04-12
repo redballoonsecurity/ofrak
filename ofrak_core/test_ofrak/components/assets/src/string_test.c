0a1714e8480f172328925bca6b3fc9eab84e8256
Marc Zalik <marc.zalik@gmail.com>
2023-04-12 15:29:48 -0400

    Indentation

--- ofrak_core/test_ofrak/components/assets/src/string_test.c
+++ ofrak_core/test_ofrak/components/assets/src/string_test.c
@@ -5,23 +5,23 @@ extern int shortString(void);
 
 // Generate bytes that look like a long ascii string (21 bytes)
 __asm__(".global longString\n\t"
-		".type longString, @function\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"push %r15\n\t"
-		"and 0, %r15\n\t"
+	".type longString, @function\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"push %r15\n\t"
+	"and 0, %r15\n\t"
 );
 
 // Generate bytes that look like a short ascii string (7 bytes)
 __asm__(".global shortString\n\t"
-		".type shortString, @function\n\t"
-		"push %r15\n\t"
-		"and 0, %r15\n\t"
+	".type shortString, @function\n\t"
+	"push %r15\n\t"
+	"and 0, %r15\n\t"
 );
 
 int main() {
