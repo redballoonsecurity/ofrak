91b91ec9d9b8082511554a05da3da46413e8bbc9
Marc Zalik <marc.zalik@gmail.com>
2023-04-12 15:26:01 -0400

    Fix comment

--- ofrak_core/test_ofrak/components/assets/src/string_test.c
+++ ofrak_core/test_ofrak/components/assets/src/string_test.c
@@ -17,7 +17,7 @@ __asm__(".global longString\n\t"
 		"and 0, %r15\n\t"
 );
 
-// Generate bytes that look like a long ascii string (7 bytes)
+// Generate bytes that look like a short ascii string (7 bytes)
 __asm__(".global shortString\n\t"
 		".type shortString, @function\n\t"
 		"push %r15\n\t"
