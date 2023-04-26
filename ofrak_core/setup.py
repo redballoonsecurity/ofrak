90f5decaac5249774d56f9ce2adb255f6a7d096a
edward <edward@redballoonsecurity.com>
2023-04-26 14:02:42 -0400

    final version bumps

--- ofrak_core/setup.py
+++ ofrak_core/setup.py
@@ -40,7 +40,7 @@ def read_requirements(requirements_path):
 
 setuptools.setup(
     name="ofrak",
-    version="3.0.0rc7",
+    version="3.0.0",
     description="A binary analysis and modification platform",
     packages=setuptools.find_packages(exclude=["test_ofrak", "test_ofrak.*"]),
     package_data={
