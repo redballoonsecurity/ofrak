299f378eb8e311a2a0bb5d7037a72d498be3273f
Jacob Strieb <99368685+rbs-jacob@users.noreply.github.com>
2023-05-01 19:37:50 -0400

    Further improve component config style
    Plus small quality of life improvements

--- frontend/src/ComponentConfigNode.svelte
+++ frontend/src/ComponentConfigNode.svelte
@@ -40,7 +40,7 @@
     background: inherit;
     color: inherit;
     border: none;
-    border-bottom: 1px solid white;
+    border-bottom: 1px solid var(--main-fg-color);
     flex-grow: 1;
     margin-left: 1ch;
     /* width: 100%;
@@ -48,31 +48,18 @@
     padding: 0; */
   }
 
-  li {
-    margin-bottom: 1em;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    justify-content: space-evenly;
-    align-items: baseline;
-    align-content: left;
-    white-space: nowrap;
+  input:invalid {
+    border-bottom: 1px solid var(--comment-color);
   }
+
   input:focus {
     outline: none;
     box-shadow: inset 0 -1px 0 var(--main-fg-color);
   }
 
-  /* Hide spinner buttons for numeric inputs
-     Source: https://stackoverflow.com/questions/3790935/can-i-hide-the-html5-number-input-s-spin-box/4298216#4298216 */
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-
-  input[type="number"] {
-    -moz-appearance: textfield;
+  input:focus:invalid {
+    outline: none;
+    box-shadow: inset 0 -1px 0 var(--comment-color);
   }
 
   .input {
@@ -80,12 +67,58 @@
     padding-bottom: 0.5em;
     border-left: 1px dashed white;
   }
+
+  select {
+    background-color: var(--main-bg-color);
+    color: inherit;
+    border: 1px solid;
+    border-color: inherit;
+    border-radius: 0;
+    padding: 0.5em 1em;
+    margin-left: 0.5em;
+    margin-right: 0.5em;
+    font-size: inherit;
+    font-family: var(--font);
+    box-shadow: none;
+  }
+
+  select {
+    width: 100%;
+    margin: 0;
+    margin-bottom: 1em;
+  }
+
+  option {
+    font-family: monospace;
+  }
+
+  .boxed {
+    border: 2px solid var(--main-fg-color);
+    padding: 2em;
+    margin: 1em 0 2em 0;
+  }
 </style>
 
 <script>
   import Checkbox from "./Checkbox.svelte";
+  import { calculator } from "./helpers";
   export let node, element, optional;
-  let listElement, dictKey, dictValue, dataclassFields, unionTypeSelect;
+  let listElement,
+    dictKey,
+    dictValue,
+    dataclassFields,
+    unionTypeSelect,
+    _element;
+
+  const INT_PLACEHOLDERS = ["0x10 * 2 + 8", "(0x10 * 0x100 + 25) * 69"];
+
+  $: if (node["type"] == "builtins.int") {
+    try {
+      element = calculator.calculate(_element);
+    } catch {
+      element = undefined;
+    }
+  }
 
   if (
     node["type"] == "typing.List" ||
@@ -152,14 +185,23 @@
     {:else if node["type"] == "builtins.bytes"}
       <label>
         {nodeName}
-        <input bind:value="{element}" />
+        <input
+          pattern="([0-9a-fA-F][0-9a-fA-F])*"
+          placeholder="00deadbeef00"
+          bind:value="{element}"
+        />
       </label>
 
       <!---->
     {:else if node["type"] == "builtins.int"}
       <label>
         {nodeName}
-        <input type="number" bind:value="{element}" />
+        <input
+          placeholder="{INT_PLACEHOLDERS[
+            Math.floor(Math.random() * INT_PLACEHOLDERS.length)
+          ]}"
+          bind:value="{_element}"
+        />
       </label>
 
       <!---->
@@ -182,19 +224,20 @@
       <!---->
     {:else if node["type"] == "typing.Dict"}
       <button on:click="{addElementToDict}">Add Element</button>
-      <p>Key</p>
-      <svelte:self node="{node['args'][0]}" bind:element="{dictKey}" />
-      <p>Value</p>
-      <svelte:self node="{node['args'][1]}" bind:element="{dictValue}" />
-      {#each element as elements}
-        {elements}
-      {/each}
+      <div class="boxed">
+        <p>Key</p>
+        <svelte:self node="{node['args'][0]}" bind:element="{dictKey}" />
+        <p>Value</p>
+        <svelte:self node="{node['args'][1]}" bind:element="{dictValue}" />
+        {#each element as elements}
+          {elements}
+        {/each}
+      </div>
 
       <!---->
     {:else if node["type"] == "typing.Union"}
       <p>Select Type</p>
       <select bind:value="{unionTypeSelect}">
-        <option value="{null}">{node["args"][0]}</option>
         {#each node["args"] as arg}
           <option value="{arg}">
             {arg.type}
@@ -206,7 +249,6 @@
       <!---->
     {:else if node["enum"] != null}
       <select bind:value="{element}">
-        <option value="{null}">{node["enum"][0]}</option>
         {#each Object.entries(node["enum"]) as [name, value]}
           <option value="{node['type']}.{name}">
             {name}
