d51477e2ca3cd2a27efffe68d89f011b203536bd
Jacob Strieb <99368685+rbs-jacob@users.noreply.github.com>
2023-05-15 16:34:38 -0400

    Add initial settings pane with theme pickers

--- frontend/src/stores.js
+++ frontend/src/stores.js
@@ -1,14 +1,40 @@
+import { animals, otherColors } from "./animals.js";
+
 import { writable } from "svelte/store";
 
-/***
- * Currently selected resource.
- */
+// Currently selected resource ID
 export const selected = writable(undefined);
+
+// Currently selected resource object
 export const selectedResource = writable(undefined);
+
+// User-generated OFRAK script (array of lines)
 export const script = writable([]);
 
-/***
- * Points the OFRAK frontend to a seperate backend server
- * When empty, uses the same host and port as the frontend
- */
-export const backendUrl = "";
+export function loadSettings(forceReset) {
+  const defaultSettings = {
+    background: "#000000",
+    foreground: "#ffffff",
+    selected: otherColors[0],
+    highlight: otherColors[1],
+    comment: "#eb8e5b",
+    colors: otherColors, // animals.map(a => a.color).concat(otherColors),
+
+    // Points the OFRAK frontend to a seperate backend server. When empty, uses
+    // the same host and port as the frontend.
+    backendUrl: "",
+  };
+
+  if (forceReset) {
+    return defaultSettings;
+  }
+  try {
+    return (
+      JSON.parse(window.localStorage.getItem("settings")) || defaultSettings
+    );
+  } catch {
+    return defaultSettings;
+  }
+}
+
+export let settings = writable(loadSettings());
