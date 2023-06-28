e2fe128374b16e0477670003a7059dc39c149cf6
edward <edward@redballoonsecurity.com>
2023-06-28 18:50:34 -0400

    fix shell problems

--- disassemblers/ofrak_ghidra/ofrak_ghidra/run_ghidra_server.sh
+++ disassemblers/ofrak_ghidra/ofrak_ghidra/run_ghidra_server.sh
@@ -9,13 +9,13 @@ GHIDRA_REPO_HOST=$5
 GHIDRA_REPO_PORT=$6
 
 # cd /opt/rbs/ghidra_10.1.2_PUBLIC
-cd ${GHIDRA_PATH}
+cd "${GHIDRA_PATH}"
 ./server/svrInstall
 
 # The following command sometimes fails for unclear reasons, so we give it a few tries.
 max_retries=5
 retry_counter=0
-while ! ./server/svrAdmin -add $GHIDRA_USER; do
+while ! ./server/svrAdmin -add "$GHIDRA_USER"; do
   if [ $retry_counter -ge $max_retries ]; then
     echo "Failure running './server/svrAdmin -add $GHIDRA_USER'. Retried $retry_counter times." >&2
     exit 1
@@ -44,9 +44,9 @@ done
 
 
 ./server/ghidraSvr restart
-./support/analyzeHeadless . dummy -postScript CreateRepository.java $GHIDRA_USER $GHIDRA_PASS $GHIDRA_REPO_HOST $GHIDRA_REPO_PORT -scriptPath ${OFRAK_GHIDRA_SCRIPTS_PATH} -deleteProject -noanalysis
+./support/analyzeHeadless . dummy -postScript CreateRepository.java "$GHIDRA_USER" "$GHIDRA_PASS" "$GHIDRA_REPO_HOST" "$GHIDRA_REPO_PORT" -scriptPath "${OFRAK_GHIDRA_SCRIPTS_PATH}" -deleteProject -noanalysis
 
 # Some versions of Ghidra have a command to add permissions to users, in addition to -add
 if (./server/svrAdmin --help 2>&1 | grep "\-grant" ) then
-  ./server/svrAdmin -grant $GHIDRA_USER +w ofrak
+  ./server/svrAdmin -grant "$GHIDRA_USER" +w ofrak
 fi
