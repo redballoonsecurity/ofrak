0420ec4de8019974c286c3cda4e2dc9617912fec
edward <edward@redballoonsecurity.com>
2023-07-25 12:05:47 -0400

    convert ihex unpacker to use bincopy project

--- ofrak_core/ofrak/core/ihex.py
+++ ofrak_core/ofrak/core/ihex.py
@@ -1,21 +1,20 @@
 import logging
+import sys
 from dataclasses import dataclass
-from io import StringIO, BytesIO
 from re import match
-from typing import List, Union, Dict
+from typing import List, Union, Tuple
 
-try:
-    from intelhex import IntelHex
+from bincopy import BinFile
 
-    INTELHEX_INSTALLED = True
-except ImportError:
-    INTELHEX_INSTALLED = False
-
-from ofrak import Identifier, Analyzer, Packer, Unpacker
-from ofrak.component.unpacker import UnpackerError
-from ofrak.core import MemoryRegion
+from ofrak.component.analyzer import Analyzer
+from ofrak.component.identifier import Identifier
+from ofrak.component.packer import Packer
+from ofrak.component.unpacker import Unpacker
 from ofrak.core.binary import GenericBinary, GenericText
+from ofrak.core.program_section import ProgramSection
+from ofrak.core.program import Program
 from ofrak.resource import Resource
+from ofrak.service.resource_service_i import ResourceFilter
 from ofrak_type.range import Range
 
 LOGGER = logging.getLogger(__name__)
@@ -33,8 +32,11 @@ class Ihex(GenericBinary):
     :00000001FF
     """
 
+
+@dataclass
+class IhexProgram(Program):
     address_limits: Range
-    start_addr: Union[None, Dict[str, int]]
+    start_addr: Union[None, int]
     segments: List[Range]
 
 
@@ -43,19 +45,12 @@ class IhexAnalyzer(Analyzer[None, Ihex]):
     Extract Intel HEX parameters
     """
 
-    targets = (Ihex,)
-    outputs = (Ihex,)
+    targets = (IhexProgram,)
+    outputs = (IhexProgram,)
 
-    async def analyze(self, resource: Resource, config=None) -> Ihex:
-        ihex_data = (await resource.get_data()).decode("utf-8")
-        ihex_obj = IntelHex(StringIO(ihex_data))
-
-        return Ihex(
-            # 0x0, (await resource.get_data_length()),
-            Range(ihex_obj.minaddr(), ihex_obj.maxaddr()),
-            ihex_obj.start_addr,
-            [Range(*segment) for segment in ihex_obj.segments()],
-        )
+    async def analyze(self, resource: Resource, config: None = None) -> IhexProgram:
+        ihex_program, _ = _binfile_analysis(await resource.get_data())
+        return ihex_program
 
 
 class IhexUnpacker(Unpacker[None]):
@@ -63,23 +58,22 @@ class IhexUnpacker(Unpacker[None]):
     Extract the Intel Hex image into a GenericBinary
     """
 
-    targets = (Ihex,)
-    children = (GenericBinary, MemoryRegion)
+    targets = (Ihex, IhexProgram)
+    children = (IhexProgram, ProgramSection)
 
     async def unpack(self, resource: Resource, config=None):
-        ihex_data = (await resource.get_data()).decode("utf-8")
-        ihex_obj = IntelHex(StringIO(ihex_data))
-        bin_view = await resource.view_as(GenericBinary)
-        ihex_view = await resource.view_as(Ihex)
-        with BytesIO() as bin_io:
-            ihex_obj.tofile(bin_io, format="bin")
-            child = await resource.create_child_from_view(bin_view, data=bin_io.getvalue())
+        if resource.has_tag(Ihex):
+            ihex_program, binfile = _binfile_analysis(await resource.get_data())
 
-        for seg in ihex_view.segments:
-            segment_data_range = seg.translate(-ihex_view.address_limits.start)
-            await child.create_child_from_view(
-                MemoryRegion(seg.start, seg.length()), data_range=segment_data_range
-            )
+            await resource.create_child_from_view(ihex_program, data=bytes(binfile.as_binary()))
+
+        elif resource.has_tag(IhexProgram):
+            ihex_program = await resource.view_as(IhexProgram)
+            for seg in ihex_program.segments:
+                segment_data_range = seg.translate(-ihex_program.address_limits.start)
+                await resource.create_child_from_view(
+                    ProgramSection(seg.start, seg.length()), data_range=segment_data_range
+                )
 
 
 class IhexPacker(Packer[None]):
@@ -87,26 +81,41 @@ class IhexPacker(Packer[None]):
     Generate an Intel HEX file from an Ihex view
     """
 
-    targets = (Ihex,)
+    targets = (Ihex, IhexProgram)
 
     async def pack(self, resource: Resource, config=None) -> None:
-        ihex_view: Ihex = await resource.view_as(Ihex)
-        if ihex_view.start_addr is None or ihex_view.start_addr.get("EIP") is None:
-            raise UnpackerError("Packing without an EIP not supported")
-        bin_resource = await resource.get_only_child()
-        bin_data = await bin_resource.get_data()
-        ihex_obj = IntelHex()
-        ihex_obj.loadbin(BytesIO(bin_data))
-        with StringIO() as ihex_io:
-            ihex_obj.tofile(
-                ihex_io,
-                format="hex",
-                memory_offset=ihex_view.address_limits.start,
-                pc_address=ihex_view.start_addr["EIP"],
-            )
-            resource.queue_patch(
-                Range(0, await resource.get_data_length()), str.encode(ihex_io.getvalue())
-            )
+        if resource.has_tag(IhexProgram):
+            updated_segments = []
+            min_vaddr = sys.maxsize
+            max_vaddr = 0
+            for segment_r in await resource.get_children_as_view(
+                ProgramSection, r_filter=ResourceFilter.with_tags(ProgramSection)
+            ):
+                seg_length = await segment_r.resource.get_data_length()
+                seg_start = segment_r.virtual_address
+                updated_segments.append(Range.from_size(seg_start, seg_length))
+                min_vaddr = min(min_vaddr, seg_start)
+                max_vaddr = max(max_vaddr, seg_start + seg_length)
+            ihex_prog = await resource.view_as(IhexProgram)
+            ihex_prog.segments = updated_segments
+            ihex_prog.address_limits = Range(min_vaddr, max_vaddr)
+            resource.add_view(ihex_prog)
+
+        elif resource.has_tag(Ihex):
+            program_child = await resource.get_only_child_as_view(IhexProgram)
+            vaddr_offset = -program_child.address_limits.start
+            binfile = BinFile()
+            binfile.execution_start_address = program_child.start_addr
+            for seg in program_child.segments:
+                seg_data = await resource.get_data(seg.translate(vaddr_offset))
+                binfile.add_binary(seg_data, seg.start)
+
+            new_data = binfile.as_ihex()
+            if new_data.endswith("\n"):
+                new_data = new_data[:-1]
+            new_data = new_data.encode("utf-8")
+            old_data_len = await resource.get_data_length()
+            resource.queue_patch(Range(0, old_data_len), new_data)
 
 
 class IhexIdentifier(Identifier):
@@ -122,3 +131,15 @@ class IhexIdentifier(Identifier):
             data = await resource.get_data()
             if match(r"(\:([0-9A-F]{2}){5,})(\n|\r\n)+", data.decode("utf-8")):
                 resource.add_tag(Ihex)
+
+
+def _binfile_analysis(raw_ihex: bytes) -> Tuple[IhexProgram, BinFile]:
+    binfile = BinFile()
+    binfile.add_ihex(raw_ihex.decode("utf-8"))
+
+    ihex_program = IhexProgram(
+        Range(binfile.minimum_address, binfile.maximum_address),
+        binfile.execution_start_address,
+        [Range(segment.minimum_address, segment.maximum_address) for segment in binfile.segments],
+    )
+    return ihex_program, binfile
