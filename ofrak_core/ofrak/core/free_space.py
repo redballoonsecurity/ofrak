9cefcfa81faf334bd6c333d4727782831fe74125
Alex Reuter <alexr@redballoonsecurity.com>
2024-02-01 11:52:47 -0500

    Fixed up whitespace for black

--- ofrak_core/ofrak/core/free_space.py
+++ ofrak_core/ofrak/core/free_space.py
@@ -500,7 +500,7 @@ class FreeSpaceModifier(Modifier[FreeSpaceModifierConfig]):
         # Patch in the patch_data
         await parent.run(BinaryPatchModifier, BinaryPatchConfig(patch_offset, patch_data))
 
-	free_offset = len(config.fill) if config.fill else 0
+        free_offset = len(config.fill) if config.fill else 0
 
         # Create the FreeSpace child
         await parent.create_child_from_view(
