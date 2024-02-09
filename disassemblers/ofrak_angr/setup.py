e2a0110dd4d6c3f9971693c31111b8778ae45277
Aleksey Nogin <aleksey@redballoonsecurity.com>
2024-02-09 16:28:37 -0500

    Update to latest angr==9.2.89, which also necessitates Python >= 3.8 and capstone==5.0.0.post1

--- disassemblers/ofrak_angr/setup.py
+++ disassemblers/ofrak_angr/setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
         "Topic :: Security",
         "Typing :: Typed",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     license="Proprietary",
     license_files=["LICENSE"],
     cmdclass={"egg_info": egg_info_ex},
