60011dc4d836753272813b6209987846099df340
Aleksey Nogin <aleksey@redballoonsecurity.com>
2024-02-10 11:36:23 -0500

    Fix a weird `ofrak_binary_ninja` coverage issue
    No clue whethere there is a better fix

--- disassemblers/ofrak_binary_ninja/ofrak_binary_ninja_test/test_analyzers.py
+++ disassemblers/ofrak_binary_ninja/ofrak_binary_ninja_test/test_analyzers.py
@@ -1,5 +1,7 @@
+from ofrak_binary_ninja.components.binary_ninja_analyzer import BINJA_TOOL
 from pytest_ofrak.patterns.data_refs_analyzer import DataRefsAnalyzerTestPattern
 
 
 class TestBinjaDataRefsAnalyzer(DataRefsAnalyzerTestPattern):
-    pass
+    async def test_installed(self) -> None:
+        assert await BINJA_TOOL.is_tool_installed()
