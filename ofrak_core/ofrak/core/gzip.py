37ba179f83f4a341c305447c2fa4a66fd9d39c04
Albert Zhang <albert@redballoonsecurity.com>
2024-07-18 17:48:44 +0000

    Refactor unpack logic to separate functions

--- ofrak_core/ofrak/core/gzip.py
+++ ofrak_core/ofrak/core/gzip.py
@@ -39,43 +39,45 @@ class GzipUnpacker(Unpacker[None]):
 
     async def unpack(self, resource: Resource, config=None):
         data = await resource.get_data()
-        if len(data) < 1024 * 1024 * 4:  # Use pigz for more than 4MiB
-            # wbits > 16 handles the gzip header and footer
-            # We need to create a zlib.Decompress object for Python < 3.11
-            # to set this parameter
-            chunks = []
-
-            decompressor = zlib.decompressobj(wbits=16 + zlib.MAX_WBITS)
-            while data.startswith(b"\037\213"):
-                chunks.append(decompressor.decompress(data))
-                if decompressor.eof:
-                    break
-                data = decompressor.unused_data.lstrip(b"\0")
-
-            if not len(chunks):
-                raise ValueError("Not a gzipped file")
-
-            return await resource.create_child(tags=(GenericBinary,), data=b"".join(chunks))
+        if len(data) >= 1024 * 1024 * 4 and await PIGZ.is_tool_installed():
+            uncompressed_data = await self.unpack_with_pigz(data)
         else:
-            cmd = ["pigz", "-d"]
-            proc = await asyncio.create_subprocess_exec(
-                *cmd,
-                stdin=asyncio.subprocess.PIPE,
-                stdout=asyncio.subprocess.PIPE,
-                stderr=asyncio.subprocess.PIPE,
-            )
-            stdout, stderr = await proc.communicate(data)
-            if proc.returncode:
-                # Forward any gzip warning message and continue
-                if proc.returncode == -2 or proc.returncode == 2:
-                    LOGGER.warning(stderr)
-                else:
-                    raise CalledProcessError(returncode=proc.returncode, cmd=cmd, stderr=stderr)
+            uncompressed_data = await self.unpack_with_zlib_module(data)
+        return await resource.create_child(tags=(GenericBinary,), data=uncompressed_data)
 
-            await resource.create_child(
-                tags=(GenericBinary,),
-                data=stdout,
-            )
+    @staticmethod
+    async def unpack_with_zlib_module(data: bytes) -> bytes:
+        chunks = []
+
+        # wbits > 16 handles the gzip header and footer
+        # We need to create a zlib.Decompress object in order to use this
+        # parameter in Python < 3.11
+        decompressor = zlib.decompressobj(wbits=16 + zlib.MAX_WBITS)
+        while data.startswith(b"\037\213"):
+            chunks.append(decompressor.decompress(data))
+            if decompressor.eof:
+                break
+            data = decompressor.unused_data.lstrip(b"\0")
+
+        if not len(chunks):
+            raise ValueError("Not a gzipped file")
+
+        return b"".join(chunks)
+
+    @staticmethod
+    async def unpack_with_pigz(data: bytes) -> bytes:
+        cmd = ["pigz", "-d"]
+        proc = await asyncio.create_subprocess_exec(
+            *cmd,
+            stdin=asyncio.subprocess.PIPE,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await proc.communicate(data)
+        if proc.returncode:
+            raise CalledProcessError(returncode=proc.returncode, cmd=cmd, stderr=stderr)
+
+        return stdout
 
 
 class GzipPacker(Packer[None]):
