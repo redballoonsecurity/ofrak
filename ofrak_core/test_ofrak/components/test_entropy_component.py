ac233dffd876f16952fd1fbf0ed976456c128160
Dan Ivanovich <dan@ivanovi.ch>
2024-07-23 18:31:13 -0400

    Correct tests that act like ofrak_gpu is missing. Final TODOs: test ofrak_gpu on ARM, finish writing tests for ofrak_gpu

--- ofrak_core/test_ofrak/components/test_entropy_component.py
+++ ofrak_core/test_ofrak/components/test_entropy_component.py
@@ -1,9 +1,23 @@
-import os.path
-from unittest.mock import MagicMock, Mock, patch
+# """
+# Tests for entropy calculation and the DataSummaryAnalyzer
+
+# For example, tests for the correctness of entropy_py/c, tests that ensure they are used in the
+# absence of ofrak_gpu, and tests that ensure entropy_gpu is used before entropy_py/c would go here
+
+# Tests for ofrak_gpu's correctness, eg tests for entropy_gpu's correctness, should NOT go here.
+# The tests in this file should pass whether or not ofrak_gpu is installed in the test env, and
+# should not rely on ofrak_gpu existing. Those tests should go in ofrak_gpu/ofrak_gpu_test.ofrak_gpu.ofrak_gpu_test
+# """
+
 import pytest
-from ofrak import OFRAKContext
+import os.path
 import test_ofrak.components
+from ofrak import OFRAKContext
+from unittest.mock import MagicMock, Mock, patch
 from typing import Any
+
+from ofrak.core.entropy.entropy_py import entropy_py
+from ofrak.core.entropy.entropy_c import entropy_c
 from ofrak.core.entropy import DataSummaryAnalyzer, DataSummary
 
 DEFAULT_WINDOW_SIZE = 256  # This should match what DataSummaryAnalyzer uses as its window size
@@ -55,6 +69,12 @@ def mock_ofrak_gpu_installed():
                 yield
 
 
+@pytest.fixture
+def mock_ofrak_gpu_missing():
+    with patch("ofrak.core.entropy.entropy.GPU_MODULE_INSTALLED", False):
+        yield
+
+
 @pytest.mark.parametrize(
     "test_file_path",
     [os.path.join(test_ofrak.components.ASSETS_DIR, filename) for filename in TEST_FILES],
@@ -84,96 +104,60 @@ async def test_analyzer_gpu(ofrak_context: OFRAKContext, test_file_path):
     assert entropy == dummy_entropy_gpu(data, DEFAULT_WINDOW_SIZE, None)
 
 
-# """
-# Tests for the DataSummaryAnalyzer that expect ofrak_gpu to NOT be installed go here. See fixtures.
+@pytest.mark.parametrize(
+    "test_file_path",
+    [os.path.join(test_ofrak.components.ASSETS_DIR, filename) for filename in TEST_FILES],
+)
+@pytest.mark.usefixtures("mock_ofrak_gpu_missing")
+async def test_analyzer_standard(ofrak_context: OFRAKContext, test_file_path):
+    """
+    Only test on small files for two reasons:
 
-# For example, tests for the correctness of entropy_py/c, tests that ensure they are used in the
-# absence of ofrak_gpu, and tests that ensure entropy_c is used before entropy_py would go here.
+    1. The sampling of large files may lead to spurious test failures.
+    2. The reference method is *extremely* slow for even moderately sized files.
 
-# Tests that use ofrak_gpu, eg to test that entropy_gpu is used before entropy_py/c, don't go here;
-# see test_entropy_component_gpu.py and its fixtures.
-# """
-# import sys
-# import pytest
-# import os.path
-# from unittest.mock import patch
-# from ofrak import OFRAKContext
-# import test_ofrak.components
-# from ofrak.core.entropy.entropy_py import entropy_py
-# from ofrak.core.entropy.entropy_c import entropy_c
+    Using the mock_ofrak_gpu_missing fixture, this case tests that, in the absence of ofrak_gpu,
+    DataSummaryAnalyzer will fall back upon entropy_py/c. It also checks the correctness of
+    entropy_py and entropy_c against each other.
+    """
 
+    with open(test_file_path, "rb") as f:
+        data = f.read()
 
-# TEST_FILES = [
-#     "hello.out",
-#     "arm_reloc_relocated.elf",
-#     "flash_test_magic.bin",
-#     "hello.rar",
-#     "imx7d-sdb.dtb",
-#     "simple_arm_gcc.o.elf",
-# ]
+    c_implementation_entropy = entropy_c(data, 256, lambda s: None)
+    py_implementation_entropy = entropy_py(data, 256)
 
+    if len(data) < 256:
+        assert c_implementation_entropy == b""
+        assert py_implementation_entropy == b""
 
-# @pytest.fixture
-# def mock_no_ofrak_gpu():
-#     with patch.dict(sys.modules, {"ofrak_gpu": None}):
-#         yield
+    assert _almost_equal(
+        c_implementation_entropy, py_implementation_entropy
+    ), f"Python and C entropy implementations for {test_file_path} differ."
 
+    expected_entropy = c_implementation_entropy
 
-# @pytest.mark.parametrize(
-#     "test_file_path",
-#     [os.path.join(test_ofrak.components.ASSETS_DIR, filename) for filename in TEST_FILES],
-# )
-# async def test_analyzer_standard(ofrak_context: OFRAKContext, test_file_path, mock_no_ofrak_gpu):
-#     from ofrak.core.entropy.entropy import DataSummary, DataSummaryAnalyzer
-
-#     """
-#     Only test on small files for two reasons:
-
-#     1. The sampling of large files may lead to spurious test failures.
-#     2. The reference method is *extremely* slow for even moderately sized files.
-
-#     Using the mock_no_ofrak_gpu fixture, this case tests that, in the absence of ofrak_gpu,
-#     DataSummaryAnalyzer will fall back upon entropy_py/c. It also checks the correctness of
-#     entropy_py and entropy_c against each other.
-#     """
-
-#     with open(test_file_path, "rb") as f:
-#         data = f.read()
-
-#     c_implementation_entropy = entropy_c(data, 256, lambda s: None)
-#     py_implementation_entropy = entropy_py(data, 256)
-
-#     if len(data) < 256:
-#         assert c_implementation_entropy == b""
-#         assert py_implementation_entropy == b""
-
-#     assert _almost_equal(
-#         c_implementation_entropy, py_implementation_entropy
-#     ), f"Python and C entropy implementations for {test_file_path} differ."
-
-#     expected_entropy = c_implementation_entropy
-
-#     root = await ofrak_context.create_root_resource_from_file(test_file_path)
-#     # Due to mock_no_ofrak_gpu, we know this will fail to import entropy_gpu and will run entropy_c
-#     await root.run(DataSummaryAnalyzer)
-#     data_summary = root.get_attributes(DataSummary)
-#     entropy = data_summary.entropy_samples
-#     assert _almost_equal(
-#         entropy, expected_entropy
-#     ), f"Entropy analysis for {test_file_path} differs from reference entropy."
+    root = await ofrak_context.create_root_resource_from_file(test_file_path)
+    # Due to mock_no_ofrak_gpu, we know this will fail to import entropy_gpu and will run entropy_c
+    await root.run(DataSummaryAnalyzer)
+    data_summary = root.get_attributes(DataSummary)
+    entropy = data_summary.entropy_samples
+    assert _almost_equal(
+        entropy, expected_entropy
+    ), f"Entropy analysis for {test_file_path} differs from reference entropy."
 
 
-# def _almost_equal(bytes1: bytes, bytes2: bytes) -> bool:
-#     """
-#     Return true if each pair of bytes in each position of two byte arrays differs by no more than
-#     one. For example: `[0, 1, 2]` and `[1, 2, 3]` are almost equal. `[2, 1, 2]` and `[0, 1,
-#     2]` are not.
-#     """
-#     if len(bytes1) != len(bytes2):
-#         return False
+def _almost_equal(bytes1: bytes, bytes2: bytes) -> bool:
+    """
+    Return true if each pair of bytes in each position of two byte arrays differs by no more than
+    one. For example: `[0, 1, 2]` and `[1, 2, 3]` are almost equal. `[2, 1, 2]` and `[0, 1,
+    2]` are not.
+    """
+    if len(bytes1) != len(bytes2):
+        return False
 
-#     for i in range(len(bytes1)):
-#         if abs(bytes1[i] - bytes2[i]) > 1:
-#             print(f"Inputs differ at byte {i} ({bytes1[i]} != {bytes2[i]})")
-#             return False
-#     return True
+    for i in range(len(bytes1)):
+        if abs(bytes1[i] - bytes2[i]) > 1:
+            print(f"Inputs differ at byte {i} ({bytes1[i]} != {bytes2[i]})")
+            return False
+    return True
