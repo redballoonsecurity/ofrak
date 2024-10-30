87d9e7a475fbbd50598018e85a92234ef523d772
Jacob Strieb <99368685+rbs-jacob@users.noreply.github.com>
2024-10-30 15:39:50 -0400

    Add simple LLM test using Ollama

---
+++ ofrak_core/test_ofrak/components/test_llm_components.py
@@ -0,0 +1,50 @@
+import asyncio
+import os
+
+import pytest
+
+import test_ofrak
+from ofrak import OFRAKContext
+from ofrak.core.llm import LlmAnalyzer, LlmAnalyzerConfig
+
+
+@pytest.fixture()
+@pytest.mark.asyncio
+async def ollama():
+    async def run_task():
+        proc = None
+        try:
+            proc = await asyncio.subprocess.create_subprocess_exec("ollama", "serve")
+            await proc.communicate()
+        except asyncio.CancelledError:
+            if proc is not None:
+                proc.kill()
+
+    task = asyncio.create_task(run_task())
+    await asyncio.sleep(5)
+    yield
+    task.cancel()
+
+
+@pytest.fixture()
+@pytest.mark.asyncio
+async def model(ollama) -> str:
+    # Smallest chat model currently available on ollama
+    model_name = "qwen2.5:0.5b"
+    proc = await asyncio.subprocess.create_subprocess_exec("ollama", "pull", model_name)
+    await proc.communicate()
+    yield model_name
+
+
+async def test_llm_component(ofrak_context: OFRAKContext, model: str):
+    root_path = os.path.join(test_ofrak.components.ASSETS_DIR, "elf", "busybox_elf_exec_noscop")
+    root = await ofrak_context.create_root_resource_from_file(root_path)
+    await root.unpack()
+    await root.auto_run(all_analyzers=True)
+    await root.run(
+        LlmAnalyzer,
+        LlmAnalyzerConfig(
+            "http://localhost:11434/api/chat",
+            model,
+        ),
+    )
