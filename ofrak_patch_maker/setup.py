5cda09b7194080931bc2454a771e1a830809a19e
Albert Zhang <albert@redballoonsecurity.com>
2024-11-22 22:33:21 +0000

    Move ofrak_patch_maker, ofrak_tutorial, ofrak_type build system to pyproject.toml with setuptools

--- ofrak_patch_maker/setup.py
+++ ofrak_patch_maker/setup.py
@@ -1,5 +1,4 @@
 import setuptools
-import pkg_resources
 from setuptools.command.egg_info import egg_info
 
 
@@ -16,59 +15,4 @@ class egg_info_ex(egg_info):
         egg_info.run(self)
 
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
-# Should be the same as in build_image.py
-def read_requirements(requirements_path):
-    with open(requirements_path) as requirements_handle:
-        return [
-            str(requirement)
-            for requirement in pkg_resources.parse_requirements(requirements_handle)
-        ]
-
-
-setuptools.setup(
-    name="ofrak_patch_maker",
-    version="4.0.2",
-    description="PatchMaker tool for applying source-code patches to binaries",
-    packages=setuptools.find_packages(exclude=("ofrak_patch_maker_test",)),
-    package_data={"ofrak_patch_maker": ["py.typed"]},
-    install_requires=[
-        "ofrak_type>=2.2.0rc0,==2.*",
-    ]
-    + read_requirements("requirements.txt"),
-    extras_require={
-        "test": [
-            "fun-coverage==0.2.0",
-            "pytest",
-            "pytest-cov",
-        ]
-    },
-    author="Red Balloon Security",
-    author_email="ofrak@redballoonsecurity.com",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://ofrak.com/",
-    download_url="https://github.com/redballoonsecurity/ofrak",
-    project_urls={
-        "Documentation": "https://ofrak.com/docs/",
-        "Community License": "https://github.com/redballoonsecurity/ofrak/blob/master/LICENSE",
-        "Commercial Licensing Information": "https://ofrak.com/license/",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "License :: Other/Proprietary License",
-        "License :: Free To Use But Restricted",
-        "License :: Free For Home Use",
-        "Topic :: Security",
-        "Typing :: Typed",
-    ],
-    python_requires=">=3.7",
-    license="Proprietary",
-    license_files=["LICENSE"],
-    cmdclass={"egg_info": egg_info_ex},
-    include_package_data=True,
-)
+setuptools.setup(cmdclass={"egg_info": egg_info_ex})
