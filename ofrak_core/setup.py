db1edd6b0a145f120a52291cacb699551cf78b68
Albert Zhang <albert@redballoonsecurity.com>
2024-11-22 22:10:22 +0000

    Move ofrak_core build system to pyproject.toml with setuptools

--- ofrak_core/setup.py
+++ ofrak_core/setup.py
@@ -1,6 +1,5 @@
 import sys
 import setuptools
-import pkg_resources
 from setuptools.command.egg_info import egg_info
 from setuptools.command.build_ext import build_ext
 
@@ -45,10 +44,6 @@ class CTypesExtension(setuptools.Extension):
     pass
 
 
-with open("README.md") as f:
-    long_description = f.read()
-
-
 entropy_so = CTypesExtension(
     "ofrak.core.entropy.entropy_c",
     sources=["ofrak/core/entropy/entropy.c"],
@@ -57,68 +52,7 @@ entropy_so = CTypesExtension(
     extra_compile_args=["-O3"] if sys.platform != "win32" else ["/O2"],
 )
 
-
-# Should be the same as in build_image.py
-def read_requirements(requirements_path):
-    with open(requirements_path) as requirements_handle:
-        return [
-            str(requirement)
-            for requirement in pkg_resources.parse_requirements(requirements_handle)
-        ]
-
-
 setuptools.setup(
-    name="ofrak",
-    version="3.2.0post0",
-    description="A binary analysis and modification platform",
-    packages=setuptools.find_packages(exclude=["test_ofrak", "test_ofrak.*"]),
-    package_data={
-        "ofrak": ["py.typed"],
-    },
-    install_requires=[
-        "ofrak_io>=1.0,==1.*",
-        "ofrak_type>=2.2.0rc0,==2.*",
-        "ofrak_patch_maker>=4.0.2rc0,==4.*",
-    ]
-    + read_requirements("requirements.txt"),
-    extras_require={
-        "docs": read_requirements("requirements-docs.txt"),
-        "test": [
-            "importlib-resources",  # Needed because of https://github.com/redballoonsecurity/ofrak/issues/398
-            "ofrak_angr~=1.0",
-            "ofrak_capstone~=1.0",
-        ]
-        + read_requirements("requirements-test.txt"),
-        "non-pypi": read_requirements("requirements-non-pypi.txt"),
-    },
-    author="Red Balloon Security",
-    author_email="ofrak@redballoonsecurity.com",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://ofrak.com/",
-    download_url="https://github.com/redballoonsecurity/ofrak",
-    project_urls={
-        "Documentation": "https://ofrak.com/docs/",
-        "Community License": "https://github.com/redballoonsecurity/ofrak/blob/master/LICENSE",
-        "Commercial Licensing Information": "https://ofrak.com/license/",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "License :: Other/Proprietary License",
-        "License :: Free To Use But Restricted",
-        "License :: Free For Home Use",
-        "Topic :: Security",
-        "Typing :: Typed",
-    ],
-    python_requires=">=3.7",
-    license="Proprietary",
-    license_files=["LICENSE"],
     cmdclass={"egg_info": egg_info_ex, "build_ext": build_ext_1},
-    entry_points={
-        "ofrak.packages": ["ofrak_pkg = ofrak"],
-        "console_scripts": ["ofrak = ofrak.__main__:main"],
-    },
     ext_modules=[entropy_so],
-    include_package_data=True,
 )
