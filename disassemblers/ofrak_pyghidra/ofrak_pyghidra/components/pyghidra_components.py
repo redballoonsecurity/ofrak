b171a11c3a86f18b4346b07cc2e42a1ab172e734
Wyatt <wyatt@redballoonsecurity.com>
2024-12-15 16:07:44 -0500

    Update pyghidra code. CodeRegion, ComplexBlock, BasicBlock unpacking tests are passing

--- disassemblers/ofrak_pyghidra/ofrak_pyghidra/components/pyghidra_components.py
+++ disassemblers/ofrak_pyghidra/ofrak_pyghidra/components/pyghidra_components.py
@@ -1,12 +1,24 @@
+import asyncio
+import logging
+
+
 import pyghidra
 from ofrak.core import *
 import struct
 from tempfile import TemporaryDirectory
 import os
 
+from ofrak.core.code_region import CodeRegion
+from ofrak.core.complex_block import ComplexBlock
 
 _GHIDRA_AUTO_LOADABLE_FORMATS = [Elf, Ihex, Pe]
 
+
+def _parse_offset(java_object):
+    """
+    This parses the offset as a big int
+    """
+    return int(str(java_object.getOffsetAsBigInteger()))
 
 @dataclass
 class PyGhidraAutoLoadProject(ResourceView):
@@ -51,116 +63,98 @@ class PyGhidraUnpacker(Unpacker[None]):
             await resource.flush_data_to_disk(program_file)
             with pyghidra.open_program(program_file) as flat_api:
                 from ghidra.program.model.block import BasicBlockModel
-
-                cbs = await self.unpack_code_region(resource, program_attributes, flat_api)
-                bb_model = BasicBlockModel(flat_api.getCurrentProgram())
-                await self.get_code_regions(resource, flat_api)
-                code_regions = await resource.get_descendants_as_view(
+                ofrak_code_regions = await resource.get_descendants_as_view(
                     v_type=CodeRegion,
                     r_filter=ResourceFilter(include_self=True, tags=(CodeRegion,)),
                 )
-                for func, cb in cbs:
-                    for code_region in code_regions:
-                        if code_region.contains(cb.virtual_address):
-                            cr = code_region
+                ghidra_code_regions = list()
+                for memory_block in flat_api.getMemoryBlocks():
+                    if memory_block.isExecute():
+                        ghidra_code_regions.append(
+                            CodeRegion(
+                                _parse_offset(memory_block.getStart()),
+                                size=memory_block.getSize(),
+                            )
+                        )
+
+                ofrak_code_regions = sorted(ofrak_code_regions, key=lambda cr: cr.virtual_address)
+                backend_code_regions = sorted(ghidra_code_regions,
+                                              key=lambda cr: cr.virtual_address)
+
+                for code_region in ofrak_code_regions:
+                    relative_va = code_region.virtual_address - ofrak_code_regions[0].virtual_address
+
+                    for backend_code_region in backend_code_regions:
+                        backend_relative_va = (
+                                backend_code_region.virtual_address -
+                                backend_code_regions[0].virtual_address
+                        )
+                        if backend_relative_va == relative_va and backend_code_region.size == code_region.size:
+                            code_region.virtual_address = backend_code_region.virtual_address
                             break
-                        else:
-                            cr = None
-                    if cr is None:
-                        continue
-                    cb_resource = await cr.create_child_region(cb)
-                    basic_blocks, data_words = await self.unpack_complex_block(
-                        resource, func, program_attributes, bb_model, flat_api
-                    )
-                    cb_view = await cb_resource.view_as(ComplexBlock)
-                    if not unpack_bbs:
-                        continue
-                    for block, bb in basic_blocks:
-                        if bb.is_exit_point:
-                            exit_vaddr = None
 
-                        if bb.size == 0:
-                            raise Exception(f"Basic block 0x{bb.virtual_address:x} has no size")
-
-                        if (
-                            bb.virtual_address < cb_view.virtual_address
-                            or (bb.virtual_address + bb.size) > cb_view.end_vaddr()
-                        ):
-                            logging.warning(
-                                f"Basic Block 0x{bb.virtual_address:x} does not fall within "
-                                f"complex block {hex(cb_view.virtual_address)}-{hex(cb_view.end_vaddr())}"
-                            )
-                            continue
-                        await cb_view.create_child_region(bb)
-                    for dw in data_words:
-                        if (
-                            dw.virtual_address < cb_view.virtual_address
-                            or (dw.virtual_address + dw.size) > cb_view.end_vaddr()
-                        ):
-                            logging.warning(
-                                f"Data Word 0x{dw.virtual_address:x} does not fall within "
-                                f"complex block {hex(cb_view.virtual_address)}-{hex(cb_view.end_vaddr())}"
-                            )
-                            continue
-                        await cb_view.create_child_region(dw)
-
-    async def get_code_regions(self, resource: Resource, flat_api):
-        backend_code_regions = []
-        blocks = flat_api.getMemoryBlocks()
-        for block in blocks:
-            if block.isExecute():
-                virtual_address = self.addr_to_int(block.start)
-                cr = CodeRegion(virtual_address=virtual_address, size=block.getSize())
-                backend_code_regions.append(cr)
-        ofrak_code_regions = await resource.get_descendants_as_view(
-            v_type=CodeRegion, r_filter=ResourceFilter(tags=[CodeRegion])
-        )
-        ofrak_code_regions = sorted(ofrak_code_regions, key=lambda cr: cr.virtual_address)
-        backend_code_regions = sorted(backend_code_regions, key=lambda cr: cr.virtual_address)
-        if len(ofrak_code_regions) > 0:
-            code_region = ofrak_code_regions[0]
-            relative_va = code_region.virtual_address - ofrak_code_regions[0].virtual_address
-
-            for backend_cr in backend_code_regions:
-                backend_relative_va = (
-                    backend_cr.virtual_address - backend_code_regions[0].virtual_address
+                await asyncio.gather(
+                    *[code_region.resource.save() for code_region in ofrak_code_regions]
                 )
 
-                if backend_relative_va == relative_va and backend_cr.size == code_region.size:
-                    resource.add_view(backend_cr)
-                    return
+                for code_region in ofrak_code_regions:
+                    func_cbs = await self.unpack_code_region(code_region, program_attributes, flat_api)
+                    for func, cb in func_cbs:
+                        cb_resource = await code_region.create_child_region(cb)
+                        cb_view = await cb_resource.view_as(ComplexBlock)
+                        if not unpack_bbs:
+                            continue
+                        bb_model = BasicBlockModel(flat_api.getCurrentProgram())
+                        basic_blocks, data_words = await self.unpack_complex_block(
+                            resource, func, program_attributes, bb_model, flat_api
+                        )
+                        for block, bb in basic_blocks:
+                            if bb.is_exit_point:
+                                exit_vaddr = None
 
-            LOGGER.debug(
-                f"No code region with relative offset {relative_va} and size {code_region.size} found in Ghidra"
-            )
-        else:
-            LOGGER.debug("No OFRAK code regions to match in Ghidra")
-        return
+                            if bb.size == 0:
+                                raise Exception(f"Basic block 0x{bb.virtual_address:x} has no size")
 
-    async def unpack_code_region(self, resource: Resource, program_attributes, flat_api):
+                            if (
+                                    bb.virtual_address < cb_view.virtual_address
+                                    or (bb.virtual_address + bb.size) > cb_view.end_vaddr()
+                            ):
+                                logging.warning(
+                                    f"Basic Block 0x{bb.virtual_address:x} does not fall within "
+                                    f"complex block {hex(cb_view.virtual_address)}-{hex(cb_view.end_vaddr())}"
+                                )
+                                continue
+                            await cb_view.create_child_region(bb)
+                        for dw in data_words:
+                            if (
+                                    dw.virtual_address < cb_view.virtual_address
+                                    or (dw.virtual_address + dw.size) > cb_view.end_vaddr()
+                            ):
+                                logging.warning(
+                                    f"Data Word 0x{dw.virtual_address:x} does not fall within "
+                                    f"complex block {hex(cb_view.virtual_address)}-{hex(cb_view.end_vaddr())}"
+                                )
+                                continue
+                            await cb_view.create_child_region(dw)
+
+    async def unpack_code_region(self, code_region: CodeRegion, program_attributes, flat_api):
         functions = []
-        func = flat_api.getFirstFunction()
-        while func is not None:
-            virtual_address = func.getEntryPoint().getOffset()
-            start = self.addr_to_int(func.getEntryPoint())
+        start_address = flat_api.getAddressFactory().getDefaultAddressSpace().getAddress(
+            hex(code_region.virtual_address)
+        )
+        end_address = flat_api.getAddressFactory().getDefaultAddressSpace().getAddress(
+            hex(code_region.virtual_address + code_region.size)
+        )
+        func = flat_api.getFunctionAt(start_address)
+        if func is None:
+            func = flat_api.getFunctionAfter(start_address)
+            if func is None:
+                return
+
+        while func is not None and end_address.subtract(func.getEntryPoint()) > 0:
+            virtual_address = _parse_offset(func.getEntryPoint())
+            start = _parse_offset(func.getEntryPoint())
             end, _ = self.get_last_address(func, flat_api)
-            try:
-                end = self.addr_to_int(end)
-            except:
-                import ipdb
-
-                ipdb.set_trace()
-            if program_attributes.bit_width == BitWidth.BIT_64:
-                mask = 0xFFFFFFFFFFFFFFFF
-            else:
-                mask = 0xFFFFFFFF
-            if virtual_address < 0:
-                virtual_address = abs((virtual_address ^ mask) + 1)
-            if end < 0:
-                end = abs((end ^ mask) + 1)
-            if start < 0:
-                start = abs((end ^ mask) + 1)
-
             if end is not None:
                 cb = ComplexBlock(
                     virtual_address=virtual_address,
@@ -178,7 +172,7 @@ class PyGhidraUnpacker(Unpacker[None]):
         bb_iter = bb_model.getCodeBlocksContaining(func.getBody(), flat_api.monitor)
         for block in bb_iter:
             address_range = block.getAddressRanges().next()
-            start = address_range.getMinAddress().getOffset()
+            start = _parse_offset(address_range.getMinAddress())
             size = address_range.getLength()
             exit_vaddr = None
             is_exit_point = True
@@ -190,18 +184,19 @@ class PyGhidraUnpacker(Unpacker[None]):
                 successor_bb = block.getDestinationBlock()
                 successor_bb_address_range = successor_bb.getAddressRanges().next()
                 if (
-                    successor_bb_address_range.getMinAddress().getOffset()
-                    >= func.getBody().getMinAddress().getOffset()
-                    and successor_bb_address_range.getMaxAddress().getOffset()
-                    <= func.getBody().getMaxAddress().getOffset()
+                    _parse_offset(successor_bb_address_range.getMinAddress())
+                    >= _parse_offset(func.getBody().getMinAddress())
+                    and _parse_offset(successor_bb_address_range.getMaxAddress())
+                    <= _parse_offset(func.getBody().getMaxAddress())
                 ):
                     is_exit_point = False
                     if (
                         exit_vaddr is None
-                        or successor_bb_address_range.getMinAddress().getOffset()
-                        == address_range.getMaxAddress().getOffset() + 1
+                        or _parse_offset(successor_bb_address_range.getMinAddress())
+                        == _parse_offset(address_range.getMaxAddress()) + 1
                     ):
-                        exit_vaddr = successor_bb_address_range.getMinAddress().getOffset()
+                        exit_vaddr = _parse_offset(successor_bb_address_range.getMinAddress())
+            from java.math import BigInteger
             instruction_mode = InstructionSetMode.NONE
             if program_attributes.isa == InstructionSet.ARM:
                 tmode_register = flat_api.getCurrentProgram().getRegister("TMode")
@@ -210,7 +205,7 @@ class PyGhidraUnpacker(Unpacker[None]):
                     .getProgramContext()
                     .getRegisterValue(tmode_register, address_range.getMinAddress())
                 )
-                if function_mode.getUnsignedValueIgnoreMask() == 1:
+                if function_mode.getUnsignedValueIgnoreMask().equals(BigInteger.ONE):
                     instruction_mode = InstructionSetMode.THUMB
             elif program_attributes.isa == InstructionSet.PPC:
                 vle_register = flat_api.getCurrentProgram().getRegister("vle")
@@ -219,17 +214,8 @@ class PyGhidraUnpacker(Unpacker[None]):
                     .getProgramContext()
                     .getRegisterValue(vle_register, address_range.getMinAddress())
                 )
-                if function_mode.getUnsignedValueIgnoreMask() == 1:
+                if function_mode.getUnsignedValueIgnoreMask().equals(BigInteger.ONE):
                     instruction_mode = InstructionSetMode.VLE
-
-            if program_attributes.bit_width == BitWidth.BIT_64:
-                mask = 0xFFFFFFFFFFFFFFFF
-            else:
-                mask = 0xFFFFFFFF
-            if start < 0:
-                start = abs((start ^ mask) + 1)
-            if exit_vaddr is not None and exit_vaddr < 0:
-                exit_vaddr = abs((exit_vaddr ^ mask) + 1)
 
             bb = BasicBlock(
                 virtual_address=start,
@@ -244,7 +230,7 @@ class PyGhidraUnpacker(Unpacker[None]):
 
         dws = []
         data = flat_api.getDataAt(end_code_addr)
-        while data is not None and data.getAddress() <= end_data_addr:
+        while data is not None and _parse_offset(data.getAddress()) <= end_data_addr:
             num_words = 1
             word_size = data.getLength()
             if word_size == 1:
@@ -262,14 +248,15 @@ class PyGhidraUnpacker(Unpacker[None]):
 
             format_string = program_attributes.endianness.get_struct_flag() + size_flag
 
+
             refs = [
-                self.addr_to_int(ref.getFromAddress())
+                _parse_offset(ref.getFromAddress())
                 for ref in flat_api.getReferencesTo(data.getAddress())
             ]
             for word in range(num_words):
                 dws.append(
                     DataWord(
-                        virtual_address=self.addr_to_int(data.getAddress()) + word,
+                        virtual_address=_parse_offset(data.getAddress()) + word,
                         size=data.getLength(),
                         format_string=format_string,
                         xrefs_to=tuple(refs),
@@ -278,11 +265,6 @@ class PyGhidraUnpacker(Unpacker[None]):
             data = flat_api.getDataAfter(data)
 
         return bbs, dws
-
-    def addr_to_int(self, addr):
-        if addr == None:
-            return None
-        return int(addr.toString(), 16)
 
     def get_last_address(self, func, flat_api):
         end_addr = None
@@ -310,7 +292,9 @@ class PyGhidraUnpacker(Unpacker[None]):
         while data is not None and nextFuncAddr.subtract(data.getAddress()) > 0:
             end_addr = data.getAddress().add(data.getLength())
             data = flat_api.getDataAfter(data)
-        return end_addr, end_code_addr
+        return _parse_offset(end_addr), end_code_addr
+
+
 
 
 class PyGhidraCodeRegionUnpacker(Unpacker[None]):
