354492cdefb0b339075c634a445e96efaa3bce5e
Wyatt <53830972+whyitfor@users.noreply.github.com>
2025-01-07 13:03:38 -0500

    Address MR feedback

--- ofrak_core/test_ofrak/service/conftest.py
+++ ofrak_core/test_ofrak/service/conftest.py
@@ -6,6 +6,7 @@ import pytest
 
 from ofrak import ResourceModel, ResourceAttributes
 from ofrak.core import (
+    File,
     GenericBinary,
     MemoryRegion,
     CodeRegion,
@@ -104,7 +105,12 @@ def tree3_resource_models() -> List[ResourceModel]:
     # Elf indexes are arbitrary, just to create some indexable value
     return [
         ResourceModel.create(
-            R_ID_3_ROOT, tags=(GenericBinary,), attributes=(TestIndexAttributes(5),)
+            R_ID_3_ROOT,
+            tags=(
+                File,
+                GenericBinary,
+            ),
+            attributes=(TestIndexAttributes(5),),
         ),
         ResourceModel.create(
             R_ID_3_1,
