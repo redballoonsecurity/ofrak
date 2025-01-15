76ffeb9a00d17695a6b69207b3413dd76d64763c
Aleksey Nogin <aleksey@redballoonsecurity.com>
2025-01-15 14:37:13 -0500

    Put the commented requirements in, per Wyatt

--- ofrak_core/setup.py
+++ ofrak_core/setup.py
@@ -83,7 +83,18 @@ setuptools.setup(
     + read_requirements("requirements.txt"),
     extras_require={
         "docs": read_requirements("requirements-docs.txt"),
-        "test": read_requirements("requirements-test.txt"),
+        "test": [
+            # Running the full set of tests requires these version of angr and capstone.
+            #  If building a Docker image, it should already be installed on the system.
+            #  If you are running the tests in another environment, you will need to make sure
+            #  that this version is installed from GitHub, since it depends on ofrak==3.3.0rc0
+            #  and neither of these are yet released on PyPI.
+            #  If these are not installed, some of the tests will be skipped
+            # "ofrak_capstone>=1.1.0rc0",
+            # "ofrak_angr~=1.1.0rc0",
+            # "importlib-resources",  # Needed because of https://github.com/redballoonsecurity/ofrak/issues/398
+        ]
+        + read_requirements("requirements-test.txt"),
         "non-pypi": read_requirements("requirements-non-pypi.txt"),
     },
     author="Red Balloon Security",
