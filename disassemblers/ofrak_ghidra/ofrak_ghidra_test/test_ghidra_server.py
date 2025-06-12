bd40da972f6abad5da0f082ab3b83a26319521bb
Wyatt <53830972+whyitfor@users.noreply.github.com>
2025-06-12 14:20:19 -0400

    Fix regex

--- disassemblers/ofrak_ghidra/ofrak_ghidra_test/test_ghidra_server.py
+++ disassemblers/ofrak_ghidra/ofrak_ghidra_test/test_ghidra_server.py
@@ -13,7 +13,8 @@ def _get_ghidra_server_process() -> Optional[psutil.Process]:
         cmdline = proc.cmdline()
         if (
             len(cmdline) > 1
-            and re.match("/usr/lib/jvm/*/bin/java", cmdline[0])
+            # Match any non-slash character
+            and re.match("/usr/lib/jvm/[^/]*/bin/java", cmdline[0])
             and cmdline[1] == "-Dwrapper.pidfile=/run/wrapper.ghidraSvr.pid"
         ):
             return proc
