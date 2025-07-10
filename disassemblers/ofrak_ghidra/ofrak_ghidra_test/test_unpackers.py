ff19f10e6cda635389bf8a623c2b4ba27574de5c
kmjones42 <mathjones42@gmail.com>
2025-07-10 15:16:46 -0400

    Revert removal of duplicate assets

--- disassemblers/ofrak_ghidra/ofrak_ghidra_test/test_unpackers.py
+++ disassemblers/ofrak_ghidra/ofrak_ghidra_test/test_unpackers.py
@@ -19,12 +19,7 @@ from pytest_ofrak.patterns.complex_block_unpacker import (
     ComplexBlockUnpackerTestCase,
 )
 
-ASSETS_DIR = os.path.abspath(
-    os.path.join(
-        os.path.dirname(__file__),
-        "../../ofrak_cached_disassembly/ofrak_cached_disassembly_test/assets",
-    )
-)
+ASSETS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "assets"))
 
 
 class TestGhidraCodeRegionUnpackAndVerify(CodeRegionUnpackAndVerifyPattern):
