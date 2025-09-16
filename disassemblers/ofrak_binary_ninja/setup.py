2d832a82259de479bd9ea186b71747219fb5dae6
Wyatt <wyatt@redballoonsecurity.com>
2025-09-16 16:00:23 -0400

    bninja

--- disassemblers/ofrak_binary_ninja/setup.py
+++ disassemblers/ofrak_binary_ninja/setup.py
@@ -25,12 +25,8 @@ setuptools.setup(
     author_email="ofrak@redballoonsecurity.com",
     description="OFRAK Binary Ninja Components",
     url="",  # TODO
-    packages=[
-        "ofrak_binary_ninja",
-        "ofrak_binary_ninja.components",
-        "ofrak_binary_ninja.components.blocks",
-        "ofrak_binary_ninja.components.symbols",
-    ],
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
     package_data={"ofrak_binary_ninja": ["py.typed"]},
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -39,15 +35,6 @@ setuptools.setup(
     install_requires=[
         "ofrak[test]>=3.3.0rc0",
     ],
-    extras_require={
-        "test": [
-            "fun-coverage==0.2.0",
-            "pytest",
-            "pytest-cov",
-            "pytest-asyncio==0.19.0",
-            "requests",
-        ]
-    },
     python_requires=">=3.7",
     license=license,
     cmdclass={"egg_info": egg_info_ex},
