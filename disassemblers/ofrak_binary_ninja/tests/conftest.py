d12716f7c888417705d83ac9cd9606fa4566d37e
Wyatt <wyatt@redballoonsecurity.com>
2025-09-16 11:52:37 -0400

    ofrak_binary_ninja refactors

--- disassemblers/ofrak_binary_ninja/tests/conftest.py
+++ disassemblers/ofrak_binary_ninja/tests/conftest.py
@@ -1,14 +1,7 @@
 import pytest
-
 import ofrak_binary_ninja
-from test_ofrak.components.hello_world_elf import hello_elf
 
 pytest_plugins = ["pytest_ofrak.fixtures"]
-
-
-@pytest.fixture(scope="session")
-def hello_world_elf() -> bytes:
-    return hello_elf()
 
 
 @pytest.fixture(autouse=True)
