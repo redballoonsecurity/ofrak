91368f0f1f6224bd5051961151cf27cd65689a5e
Wyatt <wyatt@redballoonsecurity.com>
2025-09-15 22:19:31 -0400

    Fix lfs issues

--- ofrak_core/tests/components/assets/src/patch_basic.c
+++ ofrak_core/tests/components/assets/src/patch_basic.c
@@ -1,3 +1,3 @@
-int main() {
-    return 3;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:346edd4aacf3ee1de1ca409a3dfae35138e375a81a66b7e0a8022399fcc664c1
+size 29
--- ofrak_core/tests/components/assets/src/patch_two_functions.c
+++ ofrak_core/tests/components/assets/src/patch_two_functions.c
@@ -1,9 +1,3 @@
-int f();
-
-int main() {
-    return f();
-}
-
-int f() {
-    return 4;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce70422b836b17e672798c1134ff8491ef5c8ea37b9220dfb3af8df0b5552517
+size 68
