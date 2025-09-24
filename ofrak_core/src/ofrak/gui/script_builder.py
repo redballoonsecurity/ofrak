d659a43ed691d1695b7b6a1c92bd2e4e3fd10e84
Paul Noalhyt <paul@redballoonsecurity.com>
2025-09-24 11:39:37 -0400

    Added comments to help user choose a Ghidra backend

--- ofrak_core/src/ofrak/gui/script_builder.py
+++ ofrak_core/src/ofrak/gui/script_builder.py
@@ -79,12 +79,12 @@ class ScriptSession:
             ofrak.discover(ofrak_capstone)
             ofrak.discover(ofrak_binary_ninja)
 
-        if False:
+        if False: # older Ghidra backend with Java server
             import ofrak_ghidra
 
             ofrak.discover(ofrak_ghidra)
 
-        if False:
+        if False: # newer PyGhidra backend
             import ofrak_pyghidra
 
             ofrak.discover(ofrak_pyghidra)
